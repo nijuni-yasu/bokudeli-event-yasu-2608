77e441e6f7b744399443af987b3bf768c939675f
Hayato Yokota <yokochie@gmail.com>
2023-05-04 21:51:09 +0900

    全パッケージを最新版に更新

--- user/vite.config.ts
+++ user/vite.config.ts
@@ -53,7 +53,6 @@ export default defineConfig({
       '@configured-variables': fileURLToPath(new URL('./src/styles/variables/_template.scss', import.meta.url)),
       '@axios': fileURLToPath(new URL('./src/plugins/axios', import.meta.url)),
       '@validators': fileURLToPath(new URL('./src/@core/utils/validators', import.meta.url)),
-      apexcharts: fileURLToPath(new URL('node_modules/apexcharts-clevision', import.meta.url)),
     },
   },
   build: {
