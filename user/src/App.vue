df7c672d4f6fabb0ada790a44c35f72df1ddca4e
Hayato Yokota <yokochie@gmail.com>
2023-05-12 21:53:06 +0900

    App から利用していない import を削除

--- user/src/App.vue
+++ user/src/App.vue
@@ -1,26 +1,15 @@
 <script setup lang="ts">
 import { useTheme } from 'vuetify'
-import { useThemeConfig } from '@core/composable/useThemeConfig'
 import { hexToRgb } from '@layouts/utils'
 
-const {
-  syncInitialLoaderTheme,
-  syncVuetifyThemeWithTheme: syncConfigThemeWithVuetifyTheme,
-  isAppRtl,
-} = useThemeConfig()
-
 const { global } = useTheme()
-
-// ℹ️ Sync current theme with initial loader theme
-syncInitialLoaderTheme()
-syncConfigThemeWithVuetifyTheme()
 </script>
 
 <template>
-  <VLocaleProvider :rtl="isAppRtl">
+  <v-locale-provider>
     <!-- ℹ️ This is required to set the background color of active nav link based on currently active global theme's primary -->
-    <VApp :style="`--v-global-theme-primary: ${hexToRgb(global.current.value.colors.primary)}`">
-      <RouterView />
-    </VApp>
-  </VLocaleProvider>
+    <v-app :style="`--v-global-theme-primary: ${hexToRgb(global.current.value.colors.primary)}`">
+      <router-view />
+    </v-app>
+  </v-locale-provider>
 </template>
