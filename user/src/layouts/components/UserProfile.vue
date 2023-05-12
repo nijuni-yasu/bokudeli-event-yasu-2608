d6144fc03e8cf4b9dc46794873d4e48e9ed4104a
Hayato Yokota <yokochie@gmail.com>
2023-05-12 21:52:08 +0900

    ユーザーメニューを編集

--- user/src/layouts/components/UserProfile.vue
+++ user/src/layouts/components/UserProfile.vue
@@ -1,82 +1,57 @@
 <script setup lang="ts">
 import avatar1 from '@images/avatars/avatar-1.png'
+
+const userId = 'bokudeli'
 </script>
 
 <template>
-  <VBadge dot location="bottom right" offset-x="3" offset-y="3" color="success">
-    <VAvatar class="cursor-pointer" color="primary" variant="tonal">
-      <VImg :src="avatar1" />
+  <v-badge dot location="bottom right" offset-x="3" offset-y="3" color="success">
+    <v-avatar class="cursor-pointer" color="primary" variant="tonal">
+      <v-img :src="avatar1" />
 
       <!-- SECTION Menu -->
-      <VMenu activator="parent" width="230" location="bottom end" offset="14px">
-        <VList>
+      <v-menu activator="parent" width="230" location="bottom end" offset="14px">
+        <v-list>
           <!-- 👉 User Avatar & Name -->
-          <VListItem>
+          <v-list-item>
             <template #prepend>
-              <VListItemAction start>
-                <VBadge dot location="bottom right" offset-x="3" offset-y="3" color="success">
-                  <VAvatar color="primary" variant="tonal">
-                    <VImg :src="avatar1" />
-                  </VAvatar>
-                </VBadge>
-              </VListItemAction>
+              <v-list-item-action start>
+                <v-badge dot location="bottom right" offset-x="3" offset-y="3" color="success">
+                  <v-avatar color="primary" variant="tonal">
+                    <v-img :src="avatar1" />
+                  </v-avatar>
+                </v-badge>
+              </v-list-item-action>
             </template>
 
-            <VListItemTitle class="font-weight-medium"> John Doe </VListItemTitle>
-            <VListItemSubtitle>Admin</VListItemSubtitle>
-          </VListItem>
-          <VDivider class="my-2" />
+            <v-list-item-title class="font-weight-medium"> John Doe </v-list-item-title>
+            <v-list-item-subtitle>Admin</v-list-item-subtitle>
+          </v-list-item>
+          <v-divider class="my-2" />
 
           <!-- 👉 Profile -->
-          <VListItem link>
+          <v-list-item :to="`/users/${userId}`">
             <template #prepend>
               <VIcon class="me-2" icon="mdi-account-outline" size="22" />
             </template>
 
-            <VListItemTitle>Profile</VListItemTitle>
-          </VListItem>
-
-          <!-- 👉 Settings -->
-          <VListItem link>
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-cog-outline" size="22" />
-            </template>
-
-            <VListItemTitle>Settings</VListItemTitle>
-          </VListItem>
-
-          <!-- 👉 Pricing -->
-          <VListItem link>
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-currency-usd" size="22" />
-            </template>
-
-            <VListItemTitle>Pricing</VListItemTitle>
-          </VListItem>
-
-          <!-- 👉 FAQ -->
-          <VListItem link>
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-help-circle-outline" size="22" />
-            </template>
-
-            <VListItemTitle>FAQ</VListItemTitle>
-          </VListItem>
+            <v-list-item-title>プロフィール</v-list-item-title>
+          </v-list-item>
 
           <!-- Divider -->
-          <VDivider class="my-2" />
+          <v-divider class="my-2" />
 
           <!-- 👉 Logout -->
-          <VListItem to="/login">
+          <v-list-item to="/login">
             <template #prepend>
               <VIcon class="me-2" icon="mdi-logout" size="22" />
             </template>
 
-            <VListItemTitle>Logout</VListItemTitle>
-          </VListItem>
-        </VList>
-      </VMenu>
+            <v-list-item-title>ログアウト</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <!-- !SECTION -->
-    </VAvatar>
-  </VBadge>
+    </v-avatar>
+  </v-badge>
 </template>
