c6a542ed39aa7f095f2b57400034e1fda592a455
Hayato Yokota <yokochie@gmail.com>
2023-06-23 20:51:53 +0900

    コミュティのイベント一覧の日付表示を修正

--- user/src/pages/community/[communityId]/index.vue
+++ user/src/pages/community/[communityId]/index.vue
@@ -9,6 +9,7 @@ import { convertDocumentDataToCommunity, convertDocumentDataToEvent } from '@/sc
 import BokudeliEvent from '@/schemes/bokudeliEvent'
 import { loadCommunityMembers } from '@/composable/loadCommunityMembers'
 import { FirestoredUser } from '@/schemes/storedUser'
+import { dateString } from '@/schemes/converter'
 
 const props = defineProps<{
   communityId: string
@@ -127,8 +128,10 @@ onMounted(async () => {
                     {{ event.eventDescription }}
                   </v-card-text>
                   <v-card-text class="text-left pb-2"> 【主催者】 {{ event.communityName }} </v-card-text>
-                  <v-card-text class="text-left pb-2"> 【注文期限】{{ event.eventDeadline }} </v-card-text>
-                  <v-card-text class="text-left pb-2"> 【開催日時】{{ event.eventStartDatetime }} </v-card-text>
+                  <v-card-text class="text-left pb-2"> 【注文期限】{{ dateString(event.eventDeadline) }} </v-card-text>
+                  <v-card-text class="text-left pb-2">
+                    【開催日時】{{ dateString(event.eventStartDatetime) }}
+                  </v-card-text>
                   <v-card-text class="text-left pb-2"> 【開催場所】{{ event.eventAddress }} </v-card-text>
                   <v-card-text class="text-left pb-2"> 【お店】 {{ event.shopName }} </v-card-text>
                   <v-card-text class="text-left pb-8"> 【定員】{{ event.eventMaxPeople }} </v-card-text>
