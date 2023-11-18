8fd7bcc7efbe7f5c4363425fa71aef2833a96c9f
Yusuke Nakashita <kokufu.ym@gmail.com>
2023-11-18 17:18:59 +0900

    [functions]shop.is_open の変化をメール

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -8,3 +8,4 @@ exports.stripe_webhook = require('./stripe-webhook').stripe_webhook;
 exports.polling = require('./sendgrid-mail').polling;
 exports.event_information = require('./sendgrid-mail').event_information;
 exports.on_event_changed = require('./sendgrid-mail').on_event_changed;
+exports.on_shop_changed = require('./sendgrid-mail').on_shop_changed;
