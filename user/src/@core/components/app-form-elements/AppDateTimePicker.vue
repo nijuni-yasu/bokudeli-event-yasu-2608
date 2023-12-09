c88bcdaecad376771f3549400f72b57fc5832b77
Yusuke Nakashita <kokufu.ym@gmail.com>
2023-12-10 08:47:19 +0900

    [user]Vuetify 3.4.6 にしたことで filterProps, filterFieldProps のインターフェースが変わったことに対応

--- user/src/@core/components/app-form-elements/AppDateTimePicker.vue
+++ user/src/@core/components/app-form-elements/AppDateTimePicker.vue
@@ -57,8 +57,8 @@ const attrs = useAttrs()
 const [rootAttrs, compAttrs] = filterInputAttrs(attrs)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const [{ modelValue: _, ...inputProps }] = VInput.filterProps(props)
-const [fieldProps] = filterFieldProps(props)
+const { modelValue: _, ...inputProps } = VInput.filterProps(props)
+const fieldProps = filterFieldProps(props)
 
 const refFlatPicker = ref()
 const { focused } = useFocus(refFlatPicker)
