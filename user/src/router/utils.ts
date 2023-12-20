b74c5001e27ea04a0e367e8a9a4a3c48c37fdd17
Yusuke Nakashita <kokufu.ym@gmail.com>
2023-12-20 14:35:02 +0900

    [user]イベント編集画面へ Event ID を送信できるように

--- user/src/router/utils.ts
+++ user/src/router/utils.ts
@@ -9,4 +9,4 @@ export const getCommunityPath = (communityAccount: string) => `/community/${comm
 export const getCommunitySettingsPath = (communityAccount: string) => `/community/${communityAccount}/settings`
 export const getEventPath = (communityAccount: string, eventId: string) =>
   `/community/${communityAccount}/events/${eventId}`
-export const getEventCreatePath = (communityAccount: string) => `/community/${communityAccount}/eventcreate`
+export const getEventCreatePath = (communityAccount: string) => `/community/${communityAccount}/events/create`
