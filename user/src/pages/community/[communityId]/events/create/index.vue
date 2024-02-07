9d71de65aad84555a07bd6c5c2b5e14767c8b39f
nijuni-yasu <naohiro.yasukawa@gmail.com>
2024-02-07 11:50:16 +0900

    [user]イベント作成時のアラート文言をIDからイベント名にする

--- user/src/pages/community/[communityId]/events/create/index.vue
+++ user/src/pages/community/[communityId]/events/create/index.vue
@@ -203,9 +203,9 @@ const sumbmit = async () => {
     return
   }
   if (route.query.id == null) {
-    window.alert(`イベントID： ${event.event_id} のイベントを新規作成しました`)
+    window.alert(`「${event.event_name}」のイベントを新規作成しました`)
   } else {
-    window.alert(`イベントID： ${event.event_id} のイベントを更新しました`)
+    window.alert(`「${event.event_name} 」のイベントを更新しました`)
   }
   router.push(getEventPath(event.community_account, event.event_id))
 }
