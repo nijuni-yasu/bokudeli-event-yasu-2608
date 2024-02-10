89ee8f5b51dbc0a6204a67669c4e9ca4fccb555d
nijuni-yasu <naohiro.yasukawa@gmail.com>
2024-02-10 14:44:01 +0900

    [user]Twitter投稿の文言修正。絵文字を追加

--- user/src/composable/shareSnsButton.ts
+++ user/src/composable/shareSnsButton.ts
@@ -1,29 +1,36 @@
 import BokudeliEvent from '@/schemes/bokudeliEvent'
 import { dateWithDayOfWeekString } from '@/schemes/converter'
 
-export const shareSnsButton = async (snsType: string , event :BokudeliEvent) => {  
+export const shareSnsButton = async (snsType: string, event: BokudeliEvent) => {
   const eventUrl = encodeURIComponent(event.url)
-  if (snsType==='twitter') {
+  if (snsType === 'twitter') {
     const baseUrl = 'https://twitter.com/intent/tweet'
-    const text =  encodeURIComponent(`${event.event_name}\n【主催】${event.community_name}\n【日時】${dateWithDayOfWeekString(event.event_start_datetime)}〜\n【お店】${event.shop_name}\n #孤食を団欒に #食事でつながる #shokujii \n`)
+    const text = encodeURIComponent(
+      `<<ひとことコメント>>\n\n📝${event.event_name}\n📍${event.community_name}\n🗓️${dateWithDayOfWeekString(
+        event.event_start_datetime,
+      )}~\n👩‍🍳${event.shop_name}\n #食事でつながる #shokujii #ショクジー \n`,
+    )
     const openUrl = `${baseUrl}?text=${text}&url=${eventUrl}`
-    window.open(openUrl, '_blank', 'width=800,height=500')    
-  } else if (snsType==='facebook') {
+    window.open(openUrl, '_blank', 'width=800,height=500')
+  } else if (snsType === 'facebook') {
     const baseUrl = 'https://www.facebook.com/sharer/sharer.php'
-    const openUrl  = `${baseUrl}?&u=${eventUrl}`
+    const openUrl = `${baseUrl}?&u=${eventUrl}`
     window.open(openUrl, '_blank', 'width=800,height=500')
-  } else if (snsType==='line') {
+  } else if (snsType === 'line') {
     const baseUrl = 'https://social-plugins.line.me/lineit/share'
-    const openUrl  = `${baseUrl}?&url=${eventUrl}?openExternalBrowser=1`
+    const openUrl = `${baseUrl}?&url=${eventUrl}?openExternalBrowser=1`
     window.open(openUrl, '_blank', 'width=800,height=500')
-  } else if (snsType==='copy') {
-    const text =  `${event.event_name}\n【主催】${event.community_name}\n【日時】${dateWithDayOfWeekString(event.event_start_datetime)}〜\n【お店】${event.shop_name}\n【注文ページ】${event.url}\n#孤食を団欒に #食事でつながる #shokujii \n`
-    navigator.clipboard.writeText(text)
+  } else if (snsType === 'copy') {
+    const text = `📝${event.event_name}\n📍${event.community_name}\n🗓️${dateWithDayOfWeekString(
+      event.event_start_datetime,
+    )}~\n👩‍🍳${event.shop_name}\n🍔${event.url}\n#食事でつながる #shokujii #ショクジー \n`
+    navigator.clipboard
+      .writeText(text)
       .then(() => {
-          alert('リンクをコピーしました')
+        alert('リンクをコピーしました')
       })
-      .catch(err => {
-          console.error('コピー失敗: ', err)
+      .catch((err) => {
+        console.error('コピー失敗: ', err)
       })
   }
 }
