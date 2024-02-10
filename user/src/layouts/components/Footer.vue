6416bb122628122e3c34b1d87446c6befbc65e61
nijuni-yasu <naohiro.yasukawa@gmail.com>
2024-02-10 21:31:16 +0900

    [user]フッターのリンクを追加。利用規約やプライバシーポリシーなど

--- user/src/layouts/components/Footer.vue
+++ user/src/layouts/components/Footer.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="h-100 d-flex justify-end">
-    <span class="d-flex align-center">
-      &copy;
-      {{ new Date().getFullYear() }}
-      nijuni inc.
-      <a href="https://nijuni.jp/" target="_blank" rel="noopener noreferrer" class="ma-2">運営会社</a>
-      <a href="https://forms.gle/z9L88Dq7vDKwbvxMA" target="noopener noreferrer" class="ma-2">お問い合わせ</a>
+  <div class="my-2 text-center">
+    <span class="align-center">
+      <a href="https://shokujii.studio.site/" target="_blank" rel="noopener noreferrer" class="footer-text">
+        shokujiiって？
+      </a>
+      <a href="https://shokujii.studio.site/restaurant" target="_blank" rel="noopener noreferrer" class="footer-text">
+        飲食店パートナーになる
+      </a>
+      <a href="https://nijuni.notion.site/shokujii-38ef325b1c5f446880bbe35bc4bbf41c" target="_blank" rel="noopener noreferrer" class="footer-text">
+        利用規約
+      </a>
+      <a href="https://nijuni.notion.site/shokujii-26a5f4507e5343329d2b7c6bea51030b" target="_blank" rel="noopener noreferrer" class="footer-text">
+        プライバシーポリシー
+      </a>
+      <a href="https://nijuni.notion.site/shokujii-FAQ-9be74ae1230a41788f0714f302714989" target="_blank" rel="noopener noreferrer" class="footer-text">
+        FAQ
+      </a>
+      <a href="https://nijuni.notion.site/a761c206d7704b5caa4058c6c3f8fff7" target="_blank" rel="noopener noreferrer" class="footer-text">
+        特定商取引法に基づく表記
+      </a>
+      <a href="https://forms.gle/z9L88Dq7vDKwbvxMA" target="_blank" rel="noopener noreferrer" class="footer-text">
+        お問い合わせ
+      </a>
+      <a href="https://nijuni.jp/" target="_blank" rel="noopener noreferrer" class="footer-text">
+        運営会社
+      </a>
     </span>
   </div>
+  <div class="mb-3 text-center">
+    &copy;
+    {{ new Date().getFullYear() }}
+    nijuni inc.
+  </div>
 </template>
+<style scoped>
+.footer-text {
+  font-size: 12px;
+  margin: 4px;
+}
+</style>
