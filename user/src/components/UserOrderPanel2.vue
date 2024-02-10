13d8f01271af753a7e7e69f08c839b90360e1156
Hayato Yokota <yokochie@gmail.com>
2024-02-10 20:03:01 +0900

    [user]他のユーザーから見たときキャンセル済みのものを表示しないように変更

--- user/src/components/UserOrderPanel2.vue
+++ user/src/components/UserOrderPanel2.vue
@@ -2,16 +2,15 @@
 import { db } from '@/firebase'
 import { collectionGroup, getDocs, orderBy, query, where } from 'firebase/firestore'
 import { functions } from '@/firebase'
-import { httpsCallable } from 'firebase/functions';
-import { getEventPath } from '@/router/utils'
-import { dateWithDayOfWeekString, priceString, convertDocumentDataToEvent } from '@/schemes/converter'
+import { httpsCallable } from 'firebase/functions'
+import { priceString, convertDocumentDataToEvent } from '@/schemes/converter'
 import BokudeliEvent from '@/schemes/bokudeliEvent'
 import OrderItem, { createEmptyOrderItem } from '@/schemes/orderItem'
 import { fixCancelOrder } from '@/composable/fixOrder'
 import ConfirmDialog from '@/components/ConfirmDialog.vue'
 import UserSuccessJoinEventDialog from '@/components/UserSuccessJoinEventDialog.vue'
 import { useRoute } from 'vue-router'
-import UserEventCard from './UserEventCard.vue';
+import UserEventCard from './UserEventCard.vue'
 
 const route = useRoute()
 
@@ -32,6 +31,14 @@ const state = reactive({
   isLoading: true,
 })
 
+const orderList = computed(() => {
+  if (props.showDetail) {
+    return state.orderList
+  } else {
+    return state.orderList.filter((order) => order.order.status !== 'canceled')
+  }
+})
+
 const loadOrderList = async () => {
   // オーダー情報の取得
   const inOrderQuery = query(
@@ -44,7 +51,7 @@ const loadOrderList = async () => {
   const orderItems = orderSnapshot.docs.map((doc) => {
     return { ...createEmptyOrderItem(), ...doc.data() }
   })
-  const filteredOrderItems = orderItems.filter(item => item.status == 'ordered' || item.status == 'canceled')
+  const filteredOrderItems = orderItems.filter((item) => item.status == 'ordered' || item.status == 'canceled')
 
   // イベント情報を引きオーダー情報とくっつける
   const convertedList = await Promise.all(
@@ -94,19 +101,19 @@ const cancelEvent = ref('')
 const cancelPrice = ref('')
 const cancelOrder = ref({} as OrderItem)
 
-const cancelConfirmDialog  = async (item: any) => {
+const cancelConfirmDialog = async (item: any) => {
   isOpenCancelConfirmDialog.value = true
   cancelEvent.value = item.event.event_name
   cancelPrice.value = priceString(item.total)
   cancelOrder.value = item.order
 }
 
-const stripeRefunds = httpsCallable(functions, 'stripe_refunds');
+const stripeRefunds = httpsCallable(functions, 'stripe_refunds')
 const startCancelProcess = async () => {
   isOpenCancelProsessDialog.value = true
   const order = cancelOrder.value
   // user_advance はstripeの支払いの場合
-  if (order.event_payment == 'user_advance' && order.payment_intent){
+  if (order.event_payment == 'user_advance' && order.payment_intent) {
     stripeRefunds({ paymentIntent: order.payment_intent, orderId: order.order_id })
       .then((result) => {
         fetchData()
@@ -114,11 +121,11 @@ const startCancelProcess = async () => {
         isOpenCancelCompleteDialog.value = true
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error('Error:', error)
         window.alert('キャンセルに失敗しました')
-      });
-  // それ以外は事前決済してないのでStripeの返金処理はなし
-  } else if (order.event_payment == 'user_on_day' || order.event_payment == 'community_bill' ) {
+      })
+    // それ以外は事前決済してないのでStripeの返金処理はなし
+  } else if (order.event_payment == 'user_on_day' || order.event_payment == 'community_bill') {
     fixCancelOrder(order)
     fetchData()
     isOpenCancelProsessDialog.value = false
@@ -145,40 +152,15 @@ if (route.query.eventId && route.query.communityAccount) {
     @click="fetchData"
   ></user-success-join-event-dialog>
   <v-row v-if="!state.isLoading" class="justify-center">
-    <v-col v-if="state.orderList.length" cols="12">
+    <v-col v-if="orderList.length" cols="12">
       <v-row>
-        <v-col v-for="item in state.orderList" :key="item.event.event_id" sm="12" md="6" lg="4" cols="12">
-          <user-event-card :order="item" :show-detail="props.showDetail" />
+        <v-col v-for="item in orderList" :key="item.event.event_id" sm="12" md="6" lg="4" cols="12">
+          <user-event-card :order="item" :show-detail="props.showDetail" @cancel="cancelConfirmDialog(item)" />
         </v-col>
       </v-row>
-      <confirm-dialog v-model="isOpenCancelConfirmDialog" :is-confirm="true" :ok-text="'キャンセルを実行する'" :cancel-text="'キャンセルしない'" :ok-click="startCancelProcess">
-        <v-card-text class="text-center py-10 text-h6">
-          キャンセル
-        </v-card-text>
-        <v-card-text class="py-5 text-h6" style="line-height: 2rem">
-          【イベント名】 {{ cancelEvent }}<br>
-          【返金額】 {{ cancelPrice }}<br>
-        </v-card-text>
-        <v-card-text class="py-5" style="line-height: 2rem">
-        注文及びイベント参加をキャンセルしますか？<br>
-        キャンセルは、イベントの注文期限まで実行可能です。<br>
-        キャンセル実行後、返金が明細書に表示されるまで5～10日かかります。<br>
-        キャンセルは、取り消しできませんのでご注意ください。
-        </v-card-text>
-      </confirm-dialog>
-      <confirm-dialog v-model="isOpenCancelProsessDialog">
-        <v-card-text class="text-center py-10 text-h6">
-          キャセル処理中...
-        </v-card-text>
-      </confirm-dialog>
-      <confirm-dialog v-model="isOpenCancelCompleteDialog">
-        <v-card-text class="text-center py-10 text-h6">
-          キャセルが完了しました
-        </v-card-text>
-      </confirm-dialog>
     </v-col>
     <!-- no result found -->
-    <v-col v-else-if="!state.orderList.length" cols="12" class="text-center">
+    <v-col v-else-if="!orderList.length" cols="12" class="text-center">
       <h4 class="mt-4">Search result not found!!</h4>
     </v-col>
   </v-row>
@@ -187,6 +169,31 @@ if (route.query.eventId && route.query.communityAccount) {
       <v-progress-circular indeterminate color="primary"></v-progress-circular>
     </v-col>
   </v-row>
+  <confirm-dialog
+    v-model="isOpenCancelConfirmDialog"
+    :is-confirm="true"
+    :ok-text="'キャンセルを実行する'"
+    :cancel-text="'キャンセルしない'"
+    :ok-click="startCancelProcess"
+  >
+    <v-card-text class="text-center py-10 text-h6"> キャンセル </v-card-text>
+    <v-card-text class="py-5 text-h6" style="line-height: 2rem">
+      【イベント名】 {{ cancelEvent }}<br />
+      【返金額】 {{ cancelPrice }}<br />
+    </v-card-text>
+    <v-card-text class="py-5" style="line-height: 2rem">
+      注文及びイベント参加をキャンセルしますか？<br />
+      キャンセルは、イベントの注文期限まで実行可能です。<br />
+      キャンセル実行後、返金が明細書に表示されるまで5～10日かかります。<br />
+      キャンセルは、取り消しできませんのでご注意ください。
+    </v-card-text>
+  </confirm-dialog>
+  <confirm-dialog v-model="isOpenCancelProsessDialog">
+    <v-card-text class="text-center py-10 text-h6"> キャンセル処理中... </v-card-text>
+  </confirm-dialog>
+  <confirm-dialog v-model="isOpenCancelCompleteDialog">
+    <v-card-text class="text-center py-10 text-h6"> キャンセルが完了しました </v-card-text>
+  </confirm-dialog>
 </template>
 
 <style lang="scss" scoped></style>
