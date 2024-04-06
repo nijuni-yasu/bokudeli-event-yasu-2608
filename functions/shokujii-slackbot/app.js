d82d8db7a80b04d554a6d6357d972fadf81969fd
Hayato Yokota <yokochie@gmail.com>
2024-04-06 17:43:39 +0900

    Slackbot のひな形部分を追加

---
+++ functions/shokujii-slackbot/app.js
@@ -0,0 +1,80 @@
+const { App, LogLevel } = require('@slack/bolt');
+
+const databaseData = {};
+const database = {
+  set: async (key, data) => {
+    databaseData[key] = data
+    console.debug('databaseData', databaseData);
+  },
+  get: async (key) => {
+    return databaseData[key];
+  },
+  delete: async (key) => {
+    delete databaseData[key];
+  }
+};
+
+// ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+const app = new App({
+  logLevel: LogLevel.DEBUG,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // clientId: process.env.SLACK_CLIENT_ID,
+  // clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: process.env.SLACK_STATE_SECRET,
+  scopes: [
+    'chat:write',
+  ],
+  installationStore: {
+    storeInstallation: async (installation) => {
+      // change the line below so it saves to your database
+      if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
+        // support for org wide app installation
+        return await database.set(installation.enterprise.id, installation);
+      }
+      if (installation.team !== undefined) {
+        // single team app installation
+        return await database.set(installation.team.id, installation);
+      }
+      throw new Error('Failed saving installation data to installationStore');
+    },
+    fetchInstallation: async (installQuery) => {
+      // change the line below so it fetches from your database
+      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+        // org wide app installation lookup
+        return await database.get(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation lookup
+        return await database.get(installQuery.teamId);
+      }
+      throw new Error('Failed fetching installation');
+    },
+    deleteInstallation: async (installQuery) => {
+      // change the line below so it deletes from your database
+      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+        // org wide app installation deletion
+        return await database.delete(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation deletion
+        return await database.delete(installQuery.teamId);
+      }
+      throw new Error('Failed to delete installation');
+    },
+  },
+  installerOptions: {
+    // If this is true, /slack/install redirects installers to the Slack authorize URL
+    // without rendering the web page with "Add to Slack" button.
+    // This flag is available in @slack/bolt v3.7 or higher
+    // directInstall: true,
+  }
+
+});
+
+(async () => {
+  // アプリを起動します
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
