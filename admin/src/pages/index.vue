00066835f0da28e84144cc8d046c340937eb75ee
Yusuke Nakashita <kokufu.ym@gmail.com>
2024-06-23 18:28:23 +0900

    [admin] トップページ追加

--- admin/src/pages/index.vue
+++ admin/src/pages/index.vue
@@ -1,1 +1,35 @@
-<template>Home</template>
+<script setup lang="ts">
+import { mdiHeartOutline, mdiCheckOutline } from '@mdi/js'
+</script>
+
+<template>
+  <v-row class="justify-center">
+    <v-col cols="12" sm="12" md="9" class="px-0">
+      <v-card flat class="mt-2">
+        <template v-slot:title>
+          <v-icon size="40" class="text--primary me-3" :icon="mdiHeartOutline" />
+          <span>{{ $t('home.title1') }}</span>
+        </template>
+        <template v-slot:text>
+          <div v-html="$t('home.message1')"></div>
+        </template>
+      </v-card>
+      <v-card flat class="mt-2">
+        <template v-slot:title>
+          <v-icon size="40" class="text--primary me-3" :icon="mdiCheckOutline" />
+          <span>{{ $t('home.title2') }}</span>
+        </template>
+        <template v-slot:text>
+          <v-table>
+            <tbody>
+              <tr v-for="(step, i) of $tm('home.todo_list')" :key="`todo_${i}`">
+                <td v-html="step[0]"></td>
+                <td v-html="step[1]"></td>
+              </tr>
+            </tbody>
+          </v-table>
+        </template>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
