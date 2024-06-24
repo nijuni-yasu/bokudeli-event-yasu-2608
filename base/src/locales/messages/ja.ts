d7cb30e8d63cb75fe4231d43173f8e23641c43c5
Yusuke Nakashita <kokufu.ym@gmail.com>
2024-06-25 07:17:52 +0900

    [base][user]ja.ts の共通項目を base に移動

--- base/src/locales/messages/ja.ts
+++ base/src/locales/messages/ja.ts
@@ -15,6 +15,13 @@ export default {
   },
   'order_deadline': '注文期限',
   'days_before': '当日 | 前日 | {n}日前',
+  'menu_disabled_reason': {
+    'finished': 'イベントが終了したため、カートに追加できません',
+    'order_closed': '注文期限をすぎました。カートに追加できません',
+    'not_accepting_order': '注文受付開始前はカートに追加できません',
+    'limit_people': '定員に達しました。カートに追加できません',
+    'sold_out': '売り切れました。カートに追加できません'
+  },
   'validator': {
     'required': '必須項目です',
     'url': 'URLの形式が正しくありません',
--- user/src/locales/messages/ja.ts
+++ user/src/locales/messages/ja.ts
@@ -7,12 +7,5 @@ export default {
     'new_event': 'イベント作成',
     'community': 'コミュニティ',
     'about': 'shokujiiって？'
-  },
-  'menu_disabled_reason': {
-    'finished': 'イベントが終了したため、カートに追加できません',
-    'order_closed': '注文期限をすぎました。カートに追加できません',
-    'not_accepting_order': '注文受付開始前はカートに追加できません',
-    'limit_people': '定員に達しました。カートに追加できません',
-    'sold_out': '売り切れました。カートに追加できません'
   }
 }
