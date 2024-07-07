64277fa4ace6118370dab51bda8dbcba669876a8
Yusuke Nakashita <kokufu.ym@gmail.com>
2024-07-07 14:06:15 +0900

    [base] Add DateInput component

---
+++ base/src/components/DateInput.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { useDate } from 'vuetify'
+import { mdiCalendar, mdiClose } from '@mdi/js'
+import { useI18n } from 'vue-i18n'
+
+const modelValue = defineModel<string | null>({
+  required: false,
+  default: null,
+})
+
+// const props = withDefaults(defineProps<Props>(), {
+// 	bar: true,
+// 	hoge: () => ['one', 'two']
+// });
+
+const props = withDefaults(
+  defineProps<{
+    readonly?: boolean
+    clearable?: boolean
+  }>(),
+  {
+    readonly: false,
+    clearable: true,
+  },
+)
+
+const adapter = useDate()
+const { d: $d } = useI18n()
+
+const _menu = ref(false)
+const menu = computed({
+  get: () => (props.readonly ? false : _menu.value),
+  set: (value) => (_menu.value = value),
+})
+const date = computed<string | null>({
+  get: () =>
+    modelValue.value != null && adapter.isValid(modelValue.value)
+      ? (adapter.parseISO(modelValue.value) as string)
+      : null,
+  set: (value) => {
+    if (value != null && adapter.isValid(value)) {
+      modelValue.value = adapter.toISO(value)
+    } else {
+      modelValue.value = null
+    }
+  },
+})
+const displayDate = computed<string>(() => (date.value == null ? '' : $d(date.value, 'date')))
+</script>
+
+<template>
+  <v-menu v-model="menu" :close-on-content-click="false" :offset-y="true" transition="scale-transition">
+    <template v-slot:activator="{ props }">
+      <v-text-field
+        :model-value="displayDate"
+        :prepend-inner-icon="mdiCalendar"
+        :append-inner-icon="clearable && modelValue != null ? mdiClose : undefined"
+        :readonly="true"
+        v-bind="{ ...props, ...$attrs }"
+        @click:append-inner.stop="modelValue = null"
+      />
+    </template>
+    <v-date-picker v-model="date" :hide-header="true" :clearable="true" @update:model-value="menu = false" />
+  </v-menu>
+</template>
