8298875289c182860fc1f4e1d4abb2357f41f951
Hayato Yokota <yokochie@gmail.com>
2024-09-07 16:16:28 +0900

    [base]X, コピーするときの文言を変更しやすい形にリファクタリング

--- base/src/composable/shareSnsButton.ts
+++ base/src/composable/shareSnsButton.ts
@@ -1,3 +1,4 @@
+import { BokudeliCommunity } from '@/schemes/bokudeliCommunity'
 import { db } from '@/firebase'
 import { doc, getDoc } from 'firebase/firestore'
 import BokudeliEvent from '@/schemes/bokudeliEvent'
@@ -11,27 +12,51 @@ const getShopAccount = async (event: BokudeliEvent): Promise<Shop> => {
   return shopDoc.data() as Shop
 }
 
+const getXPostText = (event: BokudeliEvent, community: BokudeliCommunity, shop: Shop) => {
+  const communityTwitterAccount = community.community_sns_twitter ?? ''
+  const communityText = event.community_name + (communityTwitterAccount ? ` @${communityTwitterAccount}` : '')
+  const shopText = shop.shop_name + (shop.shop_url_twitter ? ` @${shop.shop_url_twitter}` : '')
+
+  const textList = [
+    `🌟${event.event_name}`,
+    `📅${dateWithDayOfWeekString(event.event_start_datetime)}~${dateOnlyTimeString(event.event_end_datetime)}`,
+    `👥${communityText}`,
+    `🍱${shopText}`,
+    '#shokujii @shokujii_jp',
+    '',
+    '👇申し込みはこちら👇',
+  ]
+  return `${textList.join('\n')}\n`
+}
+
+const getCopyText = (event: BokudeliEvent, community: BokudeliCommunity, shop: Shop) => {
+  const textList = [
+    `🌟${event.event_name}`,
+    `👥${event.community_name}`,
+    `📅${dateWithDayOfWeekString(event.event_start_datetime)}~${dateOnlyTimeString(event.event_end_datetime)}`,
+    `⏳${dateWithDayOfWeekString(event.event_deadline_datetime)}ﾏﾃﾞ`,
+    `📍${event.event_address}`,
+    `🍱${shop.shop_name}`,
+    '#shokujii',
+    '',
+    '👇申し込みはコチラから👇',
+    `${event.url}?openExternalBrowser=1`,
+  ]
+  return `${textList.join('\n')}`
+}
+
 export const shareSnsButton = async (snsType: string, event: BokudeliEvent | null) => {
   if (event == null) {
     return
   }
 
   const communityStore = useCommunityStore(event.community_account) as CommunityStore
-  const communityTwitterAccount = communityStore.community?.community_sns_twitter ?? ''
   const shop = await getShopAccount(event)
 
   const eventUrl = encodeURIComponent(event.url)
   if (snsType === 'twitter') {
-    const startDate = dateWithDayOfWeekString(event.event_start_datetime)
-    const endTime = dateOnlyTimeString(event.event_end_datetime)
-    const community = event.community_name + (communityTwitterAccount ? ` @${communityTwitterAccount}` : '')
-
-    const shopText = shop.shop_name + (shop.shop_url_twitter ? ` @${shop.shop_url_twitter}` : '')
-
     const baseUrl = 'https://twitter.com/intent/tweet'
-    const text = encodeURIComponent(
-      `🌟${event.event_name}\n📅${startDate}~${endTime}\n👥${community}\n🍱${shopText}\n#shokujii @shokujii_jp\n\n👇申し込みはこちら👇\n`,
-    )
+    const text = encodeURIComponent(getXPostText(event, communityStore.community, shop))
     const openUrl = `${baseUrl}?text=${text}&url=${eventUrl}`
     window.open(openUrl, '_blank', 'width=800,height=500')
   } else if (snsType === 'facebook') {
@@ -43,7 +68,7 @@ export const shareSnsButton = async (snsType: string, event: BokudeliEvent | nul
     const openUrl = `${baseUrl}?&url=${eventUrl}?openExternalBrowser=1`
     window.open(openUrl, '_blank', 'width=800,height=500')
   } else if (snsType === 'copy') {
-    const text = `🌟${event.event_name}\n👥${event.community_name}\n📅${dateWithDayOfWeekString(event.event_start_datetime)}~${dateOnlyTimeString(event.event_end_datetime)}\n⏳${dateWithDayOfWeekString(event.event_deadline_datetime)}ﾏﾃﾞ\n📍${event.event_address}\n🍱${event.shop_name}\n#shokujii\n\n👇申し込みはコチラから👇\n${event.url}?openExternalBrowser=1`
+    const text = getCopyText(event, communityStore.community, shop)
     navigator.clipboard
       .writeText(text)
       .then(() => {
