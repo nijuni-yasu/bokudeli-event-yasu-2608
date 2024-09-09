82f1e877706b2e8bfadd68f5e8f434a8992dbcfe
Yusuke Nakashita <kokufu.ym@gmail.com>
2024-09-09 18:30:58 +0900

    [admin] #489 店舗設定、配送距離、最大距離を 30km に

--- admin/src/pages/shop.vue
+++ admin/src/pages/shop.vue
@@ -36,7 +36,7 @@ const makeTimeArray = (start: number, num: number) =>
 
 const SHOP_MIN_ORDERS_ARRAY = [null, ...[...Array(15)].map((_, i) => i + 1)]
 // prettier-ignore
-const SHOP_RANGE_ARRAY = [null, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 35, 40, 45, 50]
+const SHOP_RANGE_ARRAY = [null, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 const SHOP_DEADLINE_DATE_ARRAY = [...Array(3)].map((_, i) => ({ title: $t('days_before', i + 1), value: i + 1 }))
 const SHOP_DEADLINE_TIME_ARRAY = makeTimeArray(6, 72)
 const SHOP_TIME_ARRAY = [null, ...makeTimeArray(6, 73)]
