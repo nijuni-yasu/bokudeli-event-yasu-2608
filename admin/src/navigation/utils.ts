04e58175b9397a958eb11159dea74285bd7113bb
Yusuke Nakashita <kokufu.ym@gmail.com>
2024-09-20 17:45:48 +0900

    [admin] #543 管理画面のURLを orders から order に

--- admin/src/navigation/utils.ts
+++ admin/src/navigation/utils.ts
@@ -1,9 +1,9 @@
 export const getHomePath = () => '/'
 export const getShopPath = () => '/shop'
 export const getMenuPath = () => '/menu'
-export const getOrderPath = () => '/orders'
+export const getOrderPath = () => '/order'
 export const getCommunityPath = () => '/community'
 export const getEventPath = () => '/events'
-export const getOrderDetailPath = (eventId: string) => `/orders/${eventId}`
+export const getOrderDetailPath = (eventId: string) => `/order/${eventId}`
 export const getUserEventUrl = (communityId: string, eventId: string) =>
   `${import.meta.env.VITE_ORIGIN_HOST}/c/${communityId}/e/${eventId}`
