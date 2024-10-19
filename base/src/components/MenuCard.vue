a9b29c68313b995d113070c3f79c9a87eeb4616c
nijuni-yasu <naohiro.yasukawa@gmail.com>
2024-10-19 21:57:51 +0900

    [admin]メニュー設定の期間限定の表示がなかったで追加表示

--- base/src/components/MenuCard.vue
+++ base/src/components/MenuCard.vue
@@ -13,18 +13,22 @@ defineProps({
   <v-card class="card">
     <v-img :src="menu.imageUrl ?? undefined" cover aspect-ratio="1" />
 
-    <!-- title -->
-    <v-card-title class="my-1">
+    <v-card-title class="text-h5 my-2 text-wrap">
       {{ menu.name }}
     </v-card-title>
     <v-card-text class="my-1 py-0">
       {{ menu.description }}
     </v-card-text>
     <div class="spacer" />
+    <v-card-text v-if="menu.dateStart && menu.dateEnd" class="d-flex">
+      <span class="sold-out">期間限定：{{ menu.dateStart }}〜{{ menu.dateEnd }}</span>
+    </v-card-text>
+    <v-card-text v-if="menu.isSoldout" class="d-flex">
+      <span class="sold-out">売り切れ</span>
+    </v-card-text>
     <v-card-text class="d-flex">
-      <span v-if="menu.isSoldout" class="sold-out">売り切れ</span>
       <v-spacer />
-      <span class="text-h6">{{ $n(menu.price, 'currency') }}</span>
+      <span class="text-h4">{{ $n(menu.price, 'currency') }}</span>
     </v-card-text>
     <slot></slot>
   </v-card>
