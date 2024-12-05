8009336e8153a78be1d828b508c2d1d17b49d884
keita okuda <keitaokuda.0925@gmail.com>
2024-12-05 19:51:21 +0900

    [user] update RouteNamedMap

--- user/typed-router.d.ts
+++ user/typed-router.d.ts
@@ -28,6 +28,7 @@ declare module 'vue-router/auto-routes' {
     '/communitylist/': RouteRecordInfo<'/communitylist/', '/communitylist', Record<never, never>, Record<never, never>>,
     '/flyer': RouteRecordInfo<'/flyer', '/flyer', Record<never, never>, Record<never, never>>,
     '/invoice': RouteRecordInfo<'/invoice', '/invoice', Record<never, never>, Record<never, never>>,
+    '/login': RouteRecordInfo<'/login', '/login', Record<never, never>, Record<never, never>>,
     '/manage/': RouteRecordInfo<'/manage/', '/manage', Record<never, never>, Record<never, never>>,
     '/manage/community/': RouteRecordInfo<'/manage/community/', '/manage/community', Record<never, never>, Record<never, never>>,
     '/manage/community/[communityAccount]/': RouteRecordInfo<'/manage/community/[communityAccount]/', '/manage/community/:communityAccount', { communityAccount: ParamValue<true> }, { communityAccount: ParamValue<false> }>,
