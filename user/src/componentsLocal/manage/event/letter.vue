84ee0a08288a92a1e18538039868d4edb0ad67de
Yusuke Nakashita <kokufu.ym@gmail.com>
2024-12-08 15:11:25 +0900

    [user] イベント letter ページ追加

--- user/src/componentsLocal/manage/event/letter.vue
+++ user/src/componentsLocal/manage/event/letter.vue
@@ -1,1 +1,134 @@
-<template>letter</template>
+<script setup lang="ts">
+import { mdiPlus } from '@mdi/js'
+import IncrementalLoader from '@/components/IncrementalLoader.vue'
+import LetterCard from '@/components/LetterCard.vue'
+import type BokudeliEvent from '@/schemes/bokudeliEvent'
+import LetterEdit from '@/components/LetterEdit.vue'
+import type { Letter } from '@/schemes/letter'
+import { useEventStore } from '@/stores/event'
+import { useLetterListStore } from '@/stores/letterList'
+import { Timestamp } from 'firebase/firestore'
+import { useLetterStore } from '@/stores/letter'
+
+const route = useRoute()
+const router = useRouter()
+
+const eventId = route.params.eventId as string
+const letterId = route.query.letterId as string | undefined
+
+const eventStore = useEventStore(eventId)
+const event: BokudeliEvent = await new Promise((resolve) => {
+  watch(
+    () => eventStore.event,
+    (event) => {
+      if (event != null) {
+        resolve(event)
+      }
+    },
+    { immediate: true },
+  )
+})
+
+const letterListStore = useLetterListStore(event.community_account)
+const letters = computed(
+  () =>
+    letterListStore.letterStores?.flatMap((ls) =>
+      ls.letter == null || ls.letter.letter_type == 'community' ? [] : ls.letter,
+    ) ?? [],
+)
+
+const getLetter = async (letterId: string) => {
+  if (letterId === '') {
+    return {
+      community_account: event.community_account,
+      event_id: event.event_id,
+      letter_type: 'event_participant',
+      letter_title: '',
+      letter_content: '',
+      status: 'draft',
+      updated_at: Timestamp.now(),
+    }
+  }
+  const letterStore = useLetterStore(event.community_account, letterId)
+  return new Promise((resolve) => {
+    watch(
+      () => letterStore.letter,
+      (letter) => {
+        if (letter != null) {
+          resolve(letter)
+        }
+      },
+      { immediate: true },
+    )
+  })
+}
+const selectedLetter = ref<Letter | null>(letterId == null ? null : ((await getLetter(letterId)) as Letter))
+watch(
+  () => route.query.letterId,
+  async (newLetterId) => {
+    if (newLetterId == null) {
+      selectedLetter.value = null
+    } else {
+      selectedLetter.value = (await getLetter(newLetterId as string)) as Letter
+    }
+  },
+)
+const onEditClick = (letter: Letter) => {
+  router.push({ query: { letterId: letter.letter_id } })
+}
+const onDeleteClick = (letter: Letter) => {
+  letterListStore.deleteLetter(letter.letter_id!)
+}
+const onCopyClick = async (letter: Letter) => {
+  const newLetter: Letter = {
+    ...letter,
+    updated_at: Timestamp.now(),
+    status: 'draft',
+  }
+  delete newLetter.letter_id
+  delete newLetter.scheduled_at
+  delete newLetter.sent_at
+  const letterId = await letterListStore.addLetter(newLetter)
+  router.push({ query: { letterId } })
+  letterListStore.reload()
+}
+</script>
+
+<template>
+  <v-container v-if="selectedLetter == null">
+    <v-row class="justify-center">
+      <v-col md="8" sm="9" cols="12">
+        <v-btn variant="outlined" :prepend-icon="mdiPlus" @click="router.push({ query: { letterId: '' } })">
+          {{ $t('manage.new_letter') }}
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center">
+      <v-col md="8" sm="9" cols="12" v-for="letter of letters" :key="letter!.letter_id">
+        <LetterCard
+          :letter="letter"
+          @edit="onEditClick(letter)"
+          @delete="onDeleteClick(letter)"
+          @copy="onCopyClick(letter)"
+        />
+      </v-col>
+    </v-row>
+    <v-row v-show="letters.length ?? 0 !== 0" class="justify-center">
+      <v-col md="8" sm="9" cols="12" class="text-center">
+        <IncrementalLoader
+          class="my-5"
+          :total-count="letterListStore.totalCount ?? Number.MAX_SAFE_INTEGER"
+          :loaded-count="letterListStore.letterStores?.length ?? 0"
+          @load="letterListStore.next()"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-container v-else>
+    <v-row class="justify-center">
+      <v-col md="8" sm="9" cols="12">
+        <LetterEdit :letter="selectedLetter" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
