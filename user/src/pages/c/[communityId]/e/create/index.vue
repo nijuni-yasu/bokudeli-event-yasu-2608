c439912fc8bcb5fb9591575c3659e1cabc8097cd
Yusuke Nakashita <kokufu.ym@gmail.com>
2024-12-15 16:18:44 +0900

    [user] EventCreate module を汎化

--- user/src/pages/c/[communityId]/e/create/index.vue
+++ user/src/pages/c/[communityId]/e/create/index.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import EventCreate from '@/components/pages/c/[communityId]/e/create/index.vue'
+import EventEdit from '@/components/EventEdit.vue'
+import { getEventPath } from '@/router/utils'
 
-const communityId = useRoute().params.communityId as string
+const route = useRoute()
+const router = useRouter()
+
+const communityAccount = route.params.communityId as string
+const eventId = ref(route.query.id as string | undefined)
+const step = ref(route.query.step as string | undefined)
 </script>
 
 <template>
-  <EventCreate :community-id="communityId" />
+  <EventEdit
+    v-if="eventId != null"
+    :community-account="communityAccount"
+    :event-id="eventId"
+    :step="step"
+    @updated="router.push(getEventPath(communityAccount, eventId))"
+  />
 </template>
