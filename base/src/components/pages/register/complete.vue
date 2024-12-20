2ff162bfc328e3a314d0d902e7a34411e36e6fc6
keita okuda <keitaokuda.0925@gmail.com>
2024-12-21 08:32:28 +0900

    [base] 新規、既存の表示切り替え追加

--- base/src/components/pages/register/complete.vue
+++ base/src/components/pages/register/complete.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
 import logo from "@/assets/images/shokujii/shokujii_logo_wide.png";
 
+const route = useRoute()
+
+const titleLabel = ref('')
+const descriptionLabel = ref('')
+const selfButtonLabel = ref('')
+const profileLink = ref('')
+
+const isNew = computed(() => {
+  const value = route.query.new
+  return value === '1' ? 1 : 0
+})
+
+if (isNew.value === 1) {
+  titleLabel.value = 'アカウント登録完了'
+  descriptionLabel.value = 'shokujiiのアカウントの登録が完了しました 🎉\nSNSアカウント連携して、プロフィールを登録しましょう。'
+  selfButtonLabel.value = '自分でプロフィールを登録する'
+  profileLink.value = '/u/profile?isNew=1'
+} else {
+  titleLabel.value = 'プロフィール登録'
+  descriptionLabel.value = 'プロフィールが登録されていません。\nSNSアカウントと連携して、プロフィール登録を完了させよう👍'
+  selfButtonLabel.value = '自分でプロフィールを入力する'
+  profileLink.value = '/u/profile?isNew=0'
+}
+
 // TODO: X連携
 
 // TODO: Facebook連携
@@ -16,12 +40,11 @@ import logo from "@/assets/images/shokujii/shokujii_logo_wide.png";
               <v-img max-height="150" max-width="300" :src="logo"></v-img>
             </v-row>
             <v-row justify="center">
-              <h1 class="my-3 text-h3 font-weight-bold">アカウント登録完了</h1>
+              <h1 class="my-3 text-h3 font-weight-bold">{{ titleLabel }}</h1>
             </v-row>
             <v-row justify="center">
-              <p>
-                shokujiiのアカウントの登録が完了しました 🎉<br/>
-                SNSアカウント連携して、プロフィールを登録しましょう。
+              <p class="pre-line">
+                {{ descriptionLabel }}
               </p>
             </v-row>
           </v-container>
@@ -32,8 +55,8 @@ import logo from "@/assets/images/shokujii/shokujii_logo_wide.png";
           <v-btn class="mb-4" size="large" color="grey-900" block>
             Facebookと連携してプロフィール登録
           </v-btn>
-          <v-btn class="mb-4" size="large" color="grey-900" block to="/u/profile">
-            自分でプロフィールを登録する
+          <v-btn class="mb-4" size="large" color="grey-900" block :to="profileLink">
+            {{ selfButtonLabel }}
           </v-btn>
         </v-sheet>
       </v-col>
