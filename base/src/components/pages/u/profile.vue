1462d8f3b312a09a323f1f96ae29160da4a290fe
keita okuda <keitaokuda.0925@gmail.com>
2024-12-21 09:25:33 +0900

    [base] 通常時表示のレイアウト追加

--- base/src/components/pages/u/profile.vue
+++ base/src/components/pages/u/profile.vue
@@ -3,11 +3,17 @@ import { functions } from '@/firebase'
 import {connectFunctionsEmulator, httpsCallable} from 'firebase/functions'
 import logo from "@/assets/images/shokujii/shokujii_logo_wide.png";
 import {generatePassCode} from "@/utils/generatePassCode";
+import XIcon from '@/icons/x'
+import AppleIcon from '@/icons/apple.vue'
+import FacebookIcon from '@/icons/facebook.vue'
+import GoogleIcon from '@/icons/google.vue'
+
 
 // TODO: 開発用。後ほど削除すること
 connectFunctionsEmulator(functions, 'localhost', 5001);
 
 const router = useRouter()
+const route = useRoute()
 
 const isLoading = ref(false)
 
@@ -25,6 +31,13 @@ const instagramUrl = ref("");
 const websiteUrl = ref("");
 const bio = ref("");
 
+const email = ref("")
+
+const isNew = computed(() => {
+  const value = route.query.new
+  return value === '1' ? 1 : 0
+})
+
 // 画像ファイル選択処理
 const triggerFileInput = (): void => {
   fileInput.value?.click()
@@ -39,9 +52,35 @@ const readImageFiles = (files: File | File[]) => {
 }
 
 
-const submit = () => {
+const profileSubmit = () => {
   // TODO: UserBioPanel.vue, UserBioEditDialog.vueを参考にuserStore.uploadUserImageを使用してアップロード
+  try {
+    isLoading.value = true
+    isValid.value = true
+
+  } catch (error) {
+    console.warn("Error profile submit:", error);
+  } finally {
+    isLoading.value = false
+    isValid.value = false
+  }
 }
+
+const emailSubmit = () => {
+  try {
+    isLoading.value = true
+    isValid.value = true
+
+    const userEmail = email.value
+    // TODO: user idからemailを保存する処理
+  } catch (error) {
+    console.warn("Error email submit:", error);
+  } finally {
+    isLoading.value = false
+    isValid.value = false
+  }
+}
+
 </script>
 
 <template>
@@ -71,7 +110,7 @@ const submit = () => {
             <v-file-input class="d-none" accept="image/*" label="プロフィール画像" ref="fileInput" @update:model-value="readImageFiles" />
           </v-sheet>
 
-          <v-form @submit.prevent="submit">
+          <v-form v-model="isValid" @submit.prevent="profileSubmit">
             <v-sheet class="d-flex flex-column ga-7 mb-16" >
               <v-text-field
                   v-model="userUrl"
@@ -127,10 +166,71 @@ const submit = () => {
               />
 
               <v-row justify="center">
-                <v-btn class="rounded-xl" color="primary" type="submit">設定を変更する</v-btn>
+                <v-btn class="rounded-xl" color="primary" :loading="isLoading" type="submit">設定を変更する</v-btn>
               </v-row>
             </v-sheet>
           </v-form>
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="isNew !== 1" justify="center" class="mt-8">
+      <v-col md="6" class="">
+        <v-sheet class="rounded-lg py-14 px-16 text-center">
+          <h1 class="text-center">メールアドレス</h1>
+
+          <v-form v-model="isValid" @submit.prevent="emailSubmit">
+            <v-text-field
+                class=" my-12"
+                label="メールアドレス"
+                v-model="userName"
+                variant="outlined"
+                :disabled="isValid"
+            />
+            <v-btn class="rounded-xl" color="primary" :loading="isLoading" type="submit">変更する</v-btn>
+          </v-form>
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="isNew !== 1" justify="center" class="mt-8">
+      <v-col md="6" class="">
+        <v-sheet class="rounded-lg py-14 px-16">
+          <h1 class="text-center">SNSアカウント連携</h1>
+
+          <div class="d-flex justify-space-between align-center my-8">
+            <label class="align-center">
+              <v-icon :icon="GoogleIcon" size="x-large" class="me-3"/>Google
+            </label>
+            <v-btn variant="outlined" color="grey-500">連携する</v-btn>
+          </div>
+
+          <hr>
+
+          <div class="d-flex justify-space-between align-center my-8">
+            <label class="align-center">
+              <v-icon :icon="AppleIcon" size="x-large" class="me-4"/>Apple
+            </label>
+            <v-btn variant="outlined" color="grey-500">連携する</v-btn>
+          </div>
+
+          <hr>
+
+          <div class="d-flex justify-space-between align-center my-8">
+            <label class="align-center">
+              <v-icon :icon="FacebookIcon" size="x-large" class="me-3"/>Facebook
+            </label>
+            <v-btn variant="outlined" color="grey-500">連携する</v-btn>
+          </div>
+
+          <hr>
+
+          <div class="d-flex justify-space-between align-center my-8">
+            <label class="align-center">
+              <v-icon :icon="XIcon" size="x-large" class="me-3"/>Twitter
+            </label>
+            <v-btn variant="outlined" color="grey-500">連携する</v-btn>
+          </div>
         </v-sheet>
       </v-col>
     </v-row>
