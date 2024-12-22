a853808265d2954f68d03380e0542983288716b2
keita okuda <keitaokuda.0925@gmail.com>
2024-12-22 12:03:15 +0900

    [functions] pass_codeをuser_pass_codeに変更、generateFirestoreUserIdを追加

--- functions/users.js
+++ functions/users.js
@@ -1,39 +1,42 @@
 import functions from 'firebase-functions'
 import { getFirestore, Timestamp} from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
+import { v4 as uuidv4 } from 'uuid';
 
 const db = getFirestore()
 
 export const create_or_update_user = functions.region('asia-northeast1').https.onCall(async (data) => {
   try {
-    const { user_email, pass_code } = data
+    const { user_email, user_pass_code } = data
     const userSnapshot = await db.collection('users').where('user_email', '==', user_email).get()
 
     if (!userSnapshot.empty) {
       // ユーザーが存在する場合、pass_code を追加して更新
       const userDoc = userSnapshot.docs[0]
       await userDoc.ref.update({
-        pass_code: pass_code,
+        user_pass_code: user_pass_code,
         verified_at: null,
-        update_at: Timestamp.now()
+        updated_at: Timestamp.now()
       });
       console.log(`ユーザー ${user_email} にパスコードが追加されました。`)
       return { is_new: true }
     } else {
       // ユーザーが存在しない場合、新規にユーザーを作成
-      await db.collection('users').add({
+      const userId = generateFirestoreUserId()
+      await db.collection('users').doc(userId).set({
+        user_id: userId,
         user_name: '',
         user_email: user_email,
-        user_image_url: '',
+        user_image_url: null,
         user_account: null,
         user_description: null,
         user_sns_facebook: null,
         user_sns_twitter: null,
         user_sns_instagram: null,
-        pass_code: pass_code,
+        user_pass_code: user_pass_code,
         verified_at: null,
         created_at: Timestamp.now(),
-        update_at: Timestamp.now()
+        updated_at: Timestamp.now()
       });
       console.log(`新規ユーザー ${user_email} が作成され、パスコードが保存されました。`)
       return { is_new: false }
@@ -45,12 +48,12 @@ export const create_or_update_user = functions.region('asia-northeast1').https.o
 
 export const verify_pass_code = functions.region('asia-northeast1').https.onCall(async (data) => {
   try {
-    const { user_email, pass_code } = data
+    const { user_email, user_pass_code } = data
 
     const userSnapshot = await db.collection('users').where('user_email', '==', user_email).get()
     if (!userSnapshot.empty) {
       const userDoc = userSnapshot.docs[0]
-      const matchedPassCode = userDoc.data().pass_code === pass_code
+      const matchedPassCode = userDoc.data().user_pass_code === user_pass_code
 
       // カスタムエラーを早期リターン
       if (!matchedPassCode) {
@@ -58,7 +61,7 @@ export const verify_pass_code = functions.region('asia-northeast1').https.onCall
       }
 
       await userDoc.ref.update({
-        pass_code: null,
+        user_pass_code: null,
         verified_at: Timestamp.now()
       })
 
@@ -73,3 +76,5 @@ export const verify_pass_code = functions.region('asia-northeast1').https.onCall
     throw new functions.https.HttpsError('internal', 'An unexpected error occurred.')
   }
 })
+
+const generateFirestoreUserId = () => uuidv4().replace(/-/g, '').substring(0, 28);
