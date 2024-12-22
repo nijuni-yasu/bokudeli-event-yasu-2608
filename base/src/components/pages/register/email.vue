cd0220e7209c775859da23aac49e2745a056446b
keita okuda <keitaokuda.0925@gmail.com>
2024-12-22 09:20:42 +0900

    [base] メールアドレス登録ロジック追加

--- base/src/components/pages/register/email.vue
+++ base/src/components/pages/register/email.vue
@@ -1,39 +1,46 @@
 <script setup lang="ts">
 import { functions } from '@/firebase'
-import {connectFunctionsEmulator, httpsCallable} from 'firebase/functions'
+import { httpsCallable } from 'firebase/functions'
 import logo from "@/assets/images/shokujii/shokujii_logo_wide.png";
 import {generatePassCode} from "@/utils/generatePassCode";
+import { useStoreStoredUser } from '@/stores/storedUser'
+import { useUserStore, type UserStore } from '@/stores/user'
+import {FirestoredUser} from "../../../schemes/storedUser";
+import {convertFirestoredUserToStoredUser} from "../../../schemes/converter";
 
-// TODO: 開発用。後ほど削除すること
-connectFunctionsEmulator(functions, 'localhost', 5001);
+const storedUserStore = useStoreStoredUser()
+const { storedUser } = storeToRefs(useStoreStoredUser())
+const user = computed(() => {
+  const userId = storedUser.value?.userId
+  return userId == null ? null : (useUserStore(userId) as UserStore).user
+})
 
+const route = useRoute()
 const router = useRouter()
 
 const isLoading = ref(false)
-
 const isValid = ref(false)
-const email = ref('')
 
 const submit = async () => {
   isLoading.value = true
   try {
-    const userEmail = email.value
-    if (!userEmail) {
-      throw new Error('Email is required')
-    }
-
+    const firestoredUser = user.value as FirestoredUser
     const passCode = generatePassCode()
+    firestoredUser.pass_code = passCode
 
-    // TODO: ログイン済みuserに対してemail登録
+    storedUserStore.update(convertFirestoredUserToStoredUser(firestoredUser))
+
+    const userStore = useUserStore(firestoredUser.user_id) as UserStore
+    await userStore.updateUser(firestoredUser)
 
     const sendPassCode = httpsCallable(functions, "send_pass_code")
-    await sendPassCode({ user_email: userEmail, pass_code: passCode })
+    await sendPassCode({ user_email: firestoredUser.user_email, pass_code: passCode })
 
     router.push({
       path: '/pass-code',
       query: {
-        email: userEmail,
-        // new: Number() TODO: ログイン済みuserから新規か既存を取得して渡す
+        email: firestoredUser.user_email,
+        new: Number(route.query.new),
       }
     })
   } catch (error) {
@@ -45,7 +52,7 @@ const submit = async () => {
 </script>
 
 <template>
-  <v-container>
+  <v-container v-if="user !== null">
     <v-row justify="center" class="mt-16">
       <v-col md="5">
         <v-sheet class="rounded-lg py-14 px-12">
@@ -64,7 +71,7 @@ const submit = async () => {
           <v-form v-model="isValid" @submit.prevent="submit">
             <v-container class=" mb-4">
               <label class="field-label" style="font-size: 12px; font-weight: bold;">メールアドレス</label>
-              <v-text-field placeholder="example@example.com" v-model="email"/>
+              <v-text-field placeholder="example@example.com" v-model="user.user_email"/>
             </v-container>
 
             <v-btn class="mb-10" size="large" color="grey-900" block :disabled="!isValid" :loading="isLoading" type="submit">
