ea48fecc6a7f3010cb7c98f9ed6f26f1dc8baa4b
keita okuda <keitaokuda.0925@gmail.com>
2025-01-06 01:43:34 +0900

    [base] ロゴ適用

--- base/src/components/pages/login.vue
+++ base/src/components/pages/login.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { functions } from '@/firebase'
 import {httpsCallable} from 'firebase/functions'
-import logo from '@/assets/images/shokujii/shokujii_logo_wide.png'
+import logo from '@/assets/images/shokujii/shokujii_logo.png'
 import {generatePassCode} from "@/utils/generatePassCode";
 import { FirebaseError } from 'firebase/app'
 import {
@@ -259,7 +259,7 @@ const transitionJudge = async (userCredential: UserCredential) => {
         <v-sheet class="rounded-lg py-14 px-12">
           <v-container>
             <v-row justify="center" >
-              <v-img max-height="150" max-width="300" :src="logo"></v-img>
+              <v-img max-width="100" :src="logo"></v-img>
             </v-row>
             <v-row justify="center">
               <h1 class="my-3 text-h3 font-weight-bold">shokujiへようこそ</h1>
