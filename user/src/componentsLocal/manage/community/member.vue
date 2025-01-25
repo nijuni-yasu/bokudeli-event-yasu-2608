f1e98b597de7389f38ad59b76b58c3ec8efbf32e
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-01-25 23:31:00 +0900

    [user]コミュニティメンバー一覧のUI調整、参加者一覧のUI調整、v-cardを敷く

--- user/src/componentsLocal/manage/community/member.vue
+++ user/src/componentsLocal/manage/community/member.vue
@@ -65,65 +65,71 @@ const downloadCsvFile = () => {
     </v-row>
     <v-row class="justify-center">
       <v-col md="10" sm="10" cols="12">
-        <v-table>
-          <tbody>
-            <tr v-for="(member, i) of members" :key="member.user_id">
-              <td>{{ i + 1 }}</td>
-              <td class="minimum-cell">
-                <router-link :to="getUserPath(member.user_id)">
-                  <UserAvatar :user="member"></UserAvatar>
-                </router-link>
-              </td>
-              <td>
-                <router-link :to="getUserPath(member.user_id)" style="color: rgba(var(--v-theme-on-surface))">
-                  {{ member.user_name }}
-                </router-link>
-              </td>
-              <td class="minimum-cell">
-                <v-btn
-                  :class="{ hidden: member.user_sns_facebook == null }"
-                  :icon="mdiFacebook"
-                  color="#1877F2"
-                  density="compact"
-                  variant="text"
-                  @click="openNewLink(buildFacebookUrl(member.user_sns_facebook!))"
-                />
-              </td>
-              <td class="minimum-cell">
-                <v-btn
-                  :class="{ hidden: member.user_sns_twitter == null }"
-                  :icon="XIcon"
-                  color="grey-900"
-                  density="compact"
-                  variant="text"
-                  @click="openNewLink(buildTwitterUrl(member.user_sns_twitter!))"
-                />
-              </td>
-              <td class="minimum-cell">
-                <v-btn
-                  :class="{ hidden: member.user_sns_instagram == null }"
-                  density="compact"
-                  variant="text"
-                  icon=""
-                  @click="openNewLink(buildInstagramUrl(member.user_sns_instagram!))"
-                >
-                  <img :src="instagramIcon" alt="Instagram" style="height: 24px; border-radius: 20%" />
-                </v-btn>
-              </td>
-              <td>
-                {{ member.roles?.includes('manager') ? $t('manage.member.manager') : $t('manage.member.member') }}
-              </td>
-              <td>
-                <v-btn
-                  v-if="canSendEmail && !isEmpty(member.user_email) && member.user_id !== userStore.user?.user_id"
-                  :icon="mdiEmail"
-                  variant="text"
-                  @click="clickContact(member)"
-                />
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
+        <v-card class="pa-5">
+          <v-row class="justify-center">
+            <v-col md="12" sm="12" cols="12">
+              <v-table>
+                <tbody>
+                  <tr v-for="(member, i) of members" :key="member.user_id">
+                    <td>{{ i + 1 }}</td>
+                    <td class="minimum-cell">
+                      <router-link :to="getUserPath(member.user_id)">
+                        <UserAvatar :user="member"></UserAvatar>
+                      </router-link>
+                    </td>
+                    <td>
+                      <router-link :to="getUserPath(member.user_id)" style="color: rgba(var(--v-theme-on-surface))">
+                        {{ member.user_name }}
+                      </router-link>
+                    </td>
+                    <td class="minimum-cell">
+                      <v-btn
+                        :class="{ hidden: member.user_sns_facebook == null }"
+                        :icon="mdiFacebook"
+                        color="#1877F2"
+                        density="compact"
+                        variant="text"
+                        @click="openNewLink(buildFacebookUrl(member.user_sns_facebook!))"
+                      />
+                    </td>
+                    <td class="minimum-cell">
+                      <v-btn
+                        :class="{ hidden: member.user_sns_twitter == null }"
+                        :icon="XIcon"
+                        color="grey-900"
+                        density="compact"
+                        variant="text"
+                        @click="openNewLink(buildTwitterUrl(member.user_sns_twitter!))"
+                      />
+                    </td>
+                    <td class="minimum-cell">
+                      <v-btn
+                        :class="{ hidden: member.user_sns_instagram == null }"
+                        density="compact"
+                        variant="text"
+                        icon=""
+                        @click="openNewLink(buildInstagramUrl(member.user_sns_instagram!))"
+                      >
+                        <img :src="instagramIcon" alt="Instagram" style="height: 24px; border-radius: 20%" />
+                      </v-btn>
+                    </td>
+                    <td>
+                      {{ member.roles?.includes('manager') ? $t('manage.member.manager') : $t('manage.member.member') }}
+                    </td>
+                    <td>
+                      <v-btn
+                        v-if="canSendEmail && !isEmpty(member.user_email) && member.user_id !== userStore.user?.user_id"
+                        :icon="mdiEmail"
+                        variant="text"
+                        @click="clickContact(member)"
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
--- user/src/componentsLocal/manage/event/member.vue
+++ user/src/componentsLocal/manage/event/member.vue
@@ -79,83 +79,91 @@ const getDateString = (order: OrderItem) => {
 
 <template>
   <v-container>
-    <template v-for="orders in tables">
-      <v-row v-if="orders.length !== 0" :key="orders[0][0].event_id" class="justify-center">
-        <v-col md="10" sm="10" cols="12">
-          <v-col cols="12" class="text-h4">
-            <v-row> {{ $t(`manage.member.${orders[0][0].status}`) }} </v-row>
-          </v-col>
-          <v-table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th colspan="2">{{ $t('manage.member.name') }}</th>
-                <th colspan="3"></th>
-                <th>{{ $t('manage.member.order') }}</th>
-                <th>{{ $t(`manage.member.date.${orders[0][0].status}`) }}</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="([order, member], i) of orders" :key="order.order_id">
-                <td>{{ i + 1 }}</td>
-                <td class="minimum-cell">
-                  <router-link :to="getUserPath(member.user_id)">
-                    <UserAvatar :user="member"></UserAvatar>
-                  </router-link>
-                </td>
-                <td>
-                  <router-link :to="getUserPath(member.user_id)" style="color: rgba(var(--v-theme-on-surface))">
-                    {{ member.user_name }}
-                  </router-link>
-                </td>
-                <td class="minimum-cell">
-                  <v-btn
-                    v-if="member.user_sns_facebook != null"
-                    :icon="mdiFacebook"
-                    color="#1877F2"
-                    density="compact"
-                    variant="text"
-                    @click="openNewLink(buildFacebookUrl(member.user_sns_facebook))"
-                  />
-                </td>
-                <td class="minimum-cell">
-                  <v-btn
-                    v-if="member.user_sns_twitter != null"
-                    :icon="XIcon"
-                    color="grey-900"
-                    density="compact"
-                    variant="text"
-                    @click="openNewLink(buildTwitterUrl(member.user_sns_twitter))"
-                  />
-                </td>
-                <td class="minimum-cell">
-                  <v-btn
-                    v-if="member.user_sns_instagram != null"
-                    density="compact"
-                    variant="text"
-                    icon=""
-                    @click="openNewLink(buildInstagramUrl(member.user_sns_instagram))"
-                  >
-                    <img :src="instagramIcon" alt="Instagram" style="height: 24px; border-radius: 20%" />
-                  </v-btn>
-                </td>
-                <td>{{ getMenuString(order.menus) }}</td>
-                <td>{{ getDateString(order) }}</td>
-                <td>
-                  <v-btn
-                    v-if="canSendEmail && !isEmpty(member.user_email) && member.user_id !== userStore.user?.user_id"
-                    :icon="mdiEmail"
-                    variant="text"
-                    @click="clickContact(member)"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </v-table>
-        </v-col>
-      </v-row>
-    </template>
+    <v-row class="justify-center">
+      <v-col md="10" sm="12" cols="12">
+        <v-card class="pa-10">
+          <template v-for="orders in tables">
+            <v-row v-if="orders.length !== 0" :key="orders[0][0].event_id" class="justify-center">
+              <v-col md="12" sm="12" cols="12">
+                <v-col cols="12" class="text-h5 font-weight-bold mt-3">
+                  <v-row> {{ $t(`manage.member.${orders[0][0].status}`) }} </v-row>
+                </v-col>
+                <v-table>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th colspan="2">{{ $t('manage.member.name') }}</th>
+                      <th colspan="3"></th>
+                      <th>{{ $t('manage.member.order') }}</th>
+                      <th>{{ $t(`manage.member.date.${orders[0][0].status}`) }}</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="([order, member], i) of orders" :key="order.order_id">
+                      <td>{{ i + 1 }}</td>
+                      <td class="minimum-cell">
+                        <router-link :to="getUserPath(member.user_id)">
+                          <UserAvatar :user="member"></UserAvatar>
+                        </router-link>
+                      </td>
+                      <td>
+                        <router-link :to="getUserPath(member.user_id)" style="color: rgba(var(--v-theme-on-surface))">
+                          {{ member.user_name }}
+                        </router-link>
+                      </td>
+                      <td class="minimum-cell">
+                        <v-btn
+                          v-if="member.user_sns_facebook != null"
+                          :icon="mdiFacebook"
+                          color="#1877F2"
+                          density="compact"
+                          variant="text"
+                          @click="openNewLink(buildFacebookUrl(member.user_sns_facebook))"
+                        />
+                      </td>
+                      <td class="minimum-cell">
+                        <v-btn
+                          v-if="member.user_sns_twitter != null"
+                          :icon="XIcon"
+                          color="grey-900"
+                          density="compact"
+                          variant="text"
+                          @click="openNewLink(buildTwitterUrl(member.user_sns_twitter))"
+                        />
+                      </td>
+                      <td class="minimum-cell">
+                        <v-btn
+                          v-if="member.user_sns_instagram != null"
+                          density="compact"
+                          variant="text"
+                          icon=""
+                          @click="openNewLink(buildInstagramUrl(member.user_sns_instagram))"
+                        >
+                          <img :src="instagramIcon" alt="Instagram" style="height: 24px; border-radius: 20%" />
+                        </v-btn>
+                      </td>
+                      <td>{{ getMenuString(order.menus) }}</td>
+                      <td>{{ getDateString(order) }}</td>
+                      <td>
+                        <v-btn
+                          v-if="
+                            canSendEmail && !isEmpty(member.user_email) && member.user_id !== userStore.user?.user_id
+                          "
+                          :icon="mdiEmail"
+                          variant="text"
+                          @click="clickContact(member)"
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </v-table>
+              </v-col>
+            </v-row>
+          </template>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
   <EmailDialog v-if="targetMember != null" v-model="isEmailDialogOpen" :toUser="targetMember" />
 </template>
