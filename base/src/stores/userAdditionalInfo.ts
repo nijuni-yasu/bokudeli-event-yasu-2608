2c15d4b0fa1e2ffe1a4d378d1a198683a8e0e902
keita okuda <keitaokuda.0925@gmail.com>
2025-02-02 00:09:59 +0900

    [user] storesにuserAdditionalInfoを追加、認証後リダイレクトでadditionalUserInfoを取得してuserAdditionalInfoに保存

---
+++ base/src/stores/userAdditionalInfo.ts
@@ -0,0 +1,18 @@
+import { type AdditionalUserInfo } from 'firebase/auth'
+import { defineStore } from 'pinia'
+
+export const useStoreUserAdditionalInfo = defineStore('userAdditionalInfo', {
+  state: (): {
+    additionalUserInfo: AdditionalUserInfo | null
+  } => ({
+    additionalUserInfo: null,
+  }),
+  actions: {
+    update(additionalUserInfo: AdditionalUserInfo) {
+      this.additionalUserInfo = additionalUserInfo
+    },
+    reset() {
+      this.additionalUserInfo = null;
+    },
+  },
+})
--- user/src/router/index.ts
+++ user/src/router/index.ts
@@ -4,6 +4,7 @@ import {
   getAuth,
   getRedirectResult,
   onAuthStateChanged,
+  getAdditionalUserInfo,
   FacebookAuthProvider,
   GoogleAuthProvider,
   TwitterAuthProvider,
@@ -20,6 +21,7 @@ import { useEventStore, type EventStore } from '@/stores/event'
 import type BokudeliEvent from '@/schemes/bokudeliEvent'
 import { useCommunityStore, type CommunityStore } from '@/stores/community'
 import { getManageCommunityListPath } from './utils'
+import {useStoreUserAdditionalInfo} from "@/stores/userAdditionalInfo";
 
 import * as ChannelService from '@channel.io/channel-web-sdk-loader'
 
@@ -28,6 +30,13 @@ const checkUser = async (user: User | null) => {
   let userCredential: UserCredential | null = null
   try {
     userCredential = await getRedirectResult(getAuth())
+    if (userCredential) {
+      const additionalUserInfo = getAdditionalUserInfo(userCredential)
+      if (additionalUserInfo) {
+        useStoreUserAdditionalInfo().update(additionalUserInfo)
+      }
+    }
+
   } catch (error) {
     if (error instanceof FirebaseError) {
       const tokenResponse = error.customData?._tokenResponse as { providerId: string }
