b9f37efdf79a4151659e2a4ec268e58e7c1e1193
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-02-03 20:11:27 +0900

    [user]トップ画面のカルーセルを追加。LINEとnote

--- user/src/pages/index.vue
+++ user/src/pages/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import topLogo from '@/assets/images/shokujii/shokujii_logo_cover.png'
+import carouselLine from '@/assets/images/shokujii/carousel_line.png'
+import carouselNote from '@/assets/images/shokujii/carousel_note.png'
 import { getEventPath } from '@/router/utils'
 import { useEventListStore } from '@/stores/eventList'
 import { where, orderBy, Timestamp } from 'firebase/firestore'
 import EventCard from '@/components/EventCard.vue'
 import IncrementalLoader from '@/components/IncrementalLoader.vue'
 import { useDisplay } from 'vuetify'
-import { mdiCrownOutline, mdiCalendarHeart, mdiCalendarCheck } from '@mdi/js'
+import { mdiCrownOutline, mdiCalendarHeart, mdiCalendarCheck, mdiChevronLeft, mdiChevronRight } from '@mdi/js'
 
 const display = useDisplay()
 
@@ -98,16 +100,62 @@ const next = () => {
     pastEventListStore.next()
   }
 }
+function openUrl(url) {
+  window.open(url, '_blank')
+}
 </script>
 
 <template>
   <v-row class="justify-center align-center">
     <v-col md="10" cols="12">
-      <a href="https://shokujii.studio.site/" target="_blank">
-        <v-card class="d-flex align-center justify-center text-center mb-5" flat>
-          <v-img :src="topLogo" />
-        </v-card>
-      </a>
+      <v-card>
+        <v-carousel
+          cycle
+          height="250"
+          hide-delimiter-background
+          :show-arrows="true"
+        >
+        <template v-slot:prev="{ props }">
+          <v-btn
+            color="#EEEEEE"
+            size="large"
+            :icon="mdiChevronLeft"
+            variant="text"
+            @click="props.onClick"
+          ></v-btn>
+        </template>
+        <template v-slot:next="{ props }">
+          <v-btn
+            color="#EEEEEE"
+            size="large"
+            :icon="mdiChevronRight"
+            variant="text"
+            @click="props.onClick"
+          ></v-btn>
+        </template>
+          <v-carousel-item
+            cover
+            hide-delimiter
+            class="clickable-item"
+            :src="topLogo"
+            @click="openUrl('https://about.shokujii.jp/')"
+          ></v-carousel-item>
+          <v-carousel-item
+            cover
+            hide-delimiter
+            class="clickable-item"
+            :src="carouselLine"
+            @click="openUrl('https://page.line.me/468evdkp?oat_content=url&openQrModal=true')"
+          ></v-carousel-item>
+          <v-carousel-item
+            cover
+            hide-delimiter
+            class="clickable-item"
+            :src="carouselNote"
+            @click="openUrl('https://note.com/shokujii/m/mc65c92109f2b')"
+          ></v-carousel-item>
+        </v-carousel>   
+      </v-card>
       <v-row class="mb-2">
         <template v-if="popularEvents.length > 0">
           <v-col cols="12" class="text-h4 mt-8 ml-2">
@@ -185,5 +233,8 @@ const next = () => {
 .event-card {
   height: 100%;
   width: 100%;
+}
+.clickable-item {
+  cursor: pointer;
 }
 </style>
