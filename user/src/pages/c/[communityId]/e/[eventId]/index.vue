6ea78abff23db37acd2a5e6e959768720f5314d7
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-02-12 03:47:11 +0900

    [user]「食事を注文してイベント参加する」のボタンのUIを調整

--- user/src/pages/c/[communityId]/e/[eventId]/index.vue
+++ user/src/pages/c/[communityId]/e/[eventId]/index.vue
@@ -278,20 +278,20 @@ onUnmounted(() => {
     touchless
     border="0"
     color="#FFFFFF00"
-    style="height: 60px; z-index: 100; text-align: center"
+    style="height: 70px; z-index: 100; text-align: center"
   >
     <v-row class="justify-center mb-2">
-      <v-col md="8" sm="9" cols="12">
+      <v-col xl="6" lg="8" md="8" sm="9" cols="12">
         <v-btn
-          class="text-h5"
+          class="text-md-h4 text-h5 font-weight-bold"
           size="large"
           rounded="pill"
-          elevation="10"
-          :prepend-icon="mdiFoodForkDrink"
+          elevation="15"
           color="primary"
-          width="90%"
+          width="85%"
           @click="scrollToMenu"
         >
+        <v-icon :icon="mdiFoodForkDrink" class="mr-2"/>
           食事を注文してイベントに参加する
         </v-btn>
       </v-col>
