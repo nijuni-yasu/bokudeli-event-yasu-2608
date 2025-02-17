443b058bf460292667388eb6c18cead45e5c59f5
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-02-18 04:28:30 +0900

    [base]TinyMCEのイベントページ詳細のCSSを調整。リンクの色、pタグのH3タグ等のspacingや行間など

--- base/src/utils/eventDetailStyle.ts
+++ base/src/utils/eventDetailStyle.ts
@@ -2,10 +2,25 @@ const eventDetailStyle = `
   body {
     color: #2E263DB3;
   }
-
+  h3 {
+    margin-top: 24px;
+    margin-bottom: 8px;
+  }
+  p, ul, ol{
+    line-height: 2.0;
+    margin-top: 8px;
+    margin-bottom: 8px;
+  }
   a {
-    color: #56CA00;
+    color: #1AC662;
     text-decoration: none;
+  }
+  table {
+    border: 1px solid #ccc;
+    border-collapse: collapse;
+  }
+  td{
+    padding: 8px;
   }
 `
 export default eventDetailStyle
