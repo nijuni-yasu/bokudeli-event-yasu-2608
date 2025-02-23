4815ade7d285dbae4d0111811f878b38f9592f76
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-02-23 18:49:31 +0900

    [user]SlackAppの連携について画像での説明を追加

--- user/src/componentsLocal/manage/community/slackSetting.vue
+++ user/src/componentsLocal/manage/community/slackSetting.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { slackBotFunctionBaseURL } from '@/firebase'
 import { useCommunityStore, type CommunityStore } from '@/stores/community'
+import { mdiContentCopy } from '@mdi/js'
+import slackLogo from '@/assets/images/slack/slack_logo.png'
+import slackImage01 from '@/assets/images/slack/slack_image_01.png'
+import slackImage02 from '@/assets/images/slack/slack_image_02.png'
+import slackImage03 from '@/assets/images/slack/slack_image_03.png'
+
 
 const { t: $t } = useI18n()
 
@@ -36,64 +42,142 @@ const copyRemoveCommand = () => {
 
 <template>
   <v-container>
-    <v-card class="pa-12">
+    <v-card class="pa-10 mb-10">
       <v-row>
-        <v-card-title class="pl-2">
-          {{ $t('manage.slack.title') }}
-        </v-card-title>
+        <v-col cols="8" sm="3" md="2">
+          <v-img
+            class="my-3"
+            :src="slackLogo"
+          />
+        </v-col>
       </v-row>
       <v-row class="description">
         <v-card-text class="pa-0 pl-2"><div v-html="$t('manage.slack.description')" /></v-card-text>
       </v-row>
+      <v-row justify="center">
+        <v-col cols="12" md="10">
+          <v-img
+            class="my-3"
+            :src="slackImage01"
+          />
+        </v-col>
+      </v-row>
+    </v-card>
+    <v-card class="pa-10 my-10">
       <v-row>
-        <v-card-title class="pl-2 mt-10">
+        <v-card-title class="pl-2">
           {{ $t('manage.slack.setup') }}
         </v-card-title>
       </v-row>
       <v-row>
-        <v-card-text class="mt-2">
-          <v-row><div v-html="$t('manage.slack.step1')" /></v-row>
-          <v-row
-            ><a :href="slackInstallUrl" target="_blank" class="text-none">{{ slackInstallUrl }}</a></v-row
-          >
-        </v-card-text>
-      </v-row>
-      <v-row>
-        <v-card-text class="mt-4">
-          <v-row
-            ><v-col class="pa-0"><div v-html="$t('manage.slack.step2')" /></v-col
-          ></v-row>
-          <v-row class="align-center">
-            <v-col cols="8" class="pa-0"
-              ><v-text-field readonly>{{ communityAddCommand }}</v-text-field></v-col
-            >
-            <v-col cols="4" class="pa-0 pl-4"
-              ><v-btn variant="text" color="on-surface" slim class="text-none" @click="copyAddCommand">{{
-                $t('manage.slack.copy')
-              }}</v-btn></v-col
-            >
+        <v-card-text class="mt-6">
+          <v-row>
+            <v-chip color="primary" class="text-none mr-2 step-chip">
+              {{ $t('manage.slack.step1') }}
+            </v-chip>
+          </v-row>
+          <v-row>
+            <div v-html="$t('manage.slack.step1_desc')" />
+          </v-row>
+          <v-row>
+            <a :href="slackInstallUrl" target="_blank" style="font-size:20px; font-weight:bold; ">
+              {{ slackInstallUrl }}
+            </a>
           </v-row>
         </v-card-text>
       </v-row>
       <v-row>
-        <v-card-text class="pa-0 pl-2 mt-4">
-          <div v-html="$t('manage.slack.step3')" />
+        <v-card-text class="mt-6">
+          <v-row>
+            <v-chip color="primary" class="text-none mr-2 step-chip">
+              {{ $t('manage.slack.step2') }}
+            </v-chip>
+          </v-row>
+          <v-row>
+            <div v-html="$t('manage.slack.step2_desc')" />
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="8">
+              <v-img
+                class="my-3"
+                :src="slackImage02"
+              />
+            </v-col>
+          </v-row>
         </v-card-text>
       </v-row>
       <v-row>
-        <v-card-text class="mt-4">
+        <v-card-text class="mt-6">
+          <v-row>
+            <v-chip color="primary" class="text-none mr-2 step-chip">
+              {{ $t('manage.slack.step3') }}
+            </v-chip>
+          </v-row>
+          <v-row>
+            <div v-html="$t('manage.slack.step3_desc')" />
+          </v-row>
+          <v-row class="align-center">
+            <v-col cols="12" md="8" class="pa-1"
+              ><v-text-field readonly>{{ communityAddCommand }}</v-text-field></v-col
+            >
+            <v-col cols="4" class="pa-1 pl-3">
+              <v-btn
+                variant="outlined"
+                :prepend-icon="mdiContentCopy"
+                @click="copyAddCommand"
+              >
+                {{ $t('manage.slack.copy')}}
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="8">
+              <v-img
+                class="my-3"
+                :src="slackImage03"
+              />
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-row>
+      <v-row>
+        <v-card-text class="mt-6">
+          <v-row>
+            <v-chip color="primary" class="text-none mr-2 step-chip">
+              {{ $t('manage.slack.step4') }}
+            </v-chip>
+          </v-row>
+          <v-row>
+            <div v-html="$t('manage.slack.step4_desc')" />
+          </v-row>
+        </v-card-text>
+      </v-row>
+
+      <!-- <v-divider class="mt-10 mb-5" :thickness="5"/> -->
+
+      <v-row>
+        <v-card-text class="mt-6">
+          <v-row>
+            <v-chip color="primary" class="text-none mr-2 step-chip">
+              {{ $t('manage.slack.step5') }}
+            </v-chip>
+          </v-row>
           <v-row
-            ><v-col class="pa-0"><div v-html="$t('manage.slack.step4')" /></v-col
+            ><v-col class="pa-0"><div v-html="$t('manage.slack.step5_desc')" /></v-col
           ></v-row>
           <v-row class="align-center">
-            <v-col cols="8" class="pa-0"
+            <v-col cols="12" md="8" class="pa-1"
               ><v-text-field readonly>{{ communityRemoveCommand }}</v-text-field></v-col
             >
-            <v-col cols="4" class="pa-0 pl-4"
-              ><v-btn variant="text" color="on-surface" slim class="text-none" @click="copyRemoveCommand">{{
-                $t('manage.slack.copy')
-              }}</v-btn></v-col
-            >
+            <v-col cols="4" class="pa-1 pl-3">
+              <v-btn
+                variant="outlined"
+                :prepend-icon="mdiContentCopy"
+                @click="copyRemoveCommand"
+              >
+                {{ $t('manage.slack.copy')}}
+              </v-btn>
+            </v-col>
           </v-row>
         </v-card-text>
       </v-row>
@@ -130,5 +214,8 @@ const copyRemoveCommand = () => {
   text-align: left;
   text-underline-position: from-font;
   text-decoration-skip-ink: none;
+}
+.step-chip{
+  font-size:20px;
 }
 </style>
