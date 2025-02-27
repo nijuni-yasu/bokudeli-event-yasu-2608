7e9cb93e34e0e5fa01f6df82b658b15d668ab382
Yusuke Nakashita <kokufu.ym@gmail.com>
2025-02-27 16:12:36 +0900

    [functions] sendgrid-mail から共通化する項目を utils に移動

--- functions/sendgrid-mail.js
+++ functions/sendgrid-mail.js
@@ -5,11 +5,8 @@ import ja from 'date-fns/locale/ja'
 import sgMail from '@sendgrid/mail'
 import { convertTruncateText } from './utils/converter.js'
 import { makeIcs } from './make-ics.js'
-
-// 環境変数の方がよいかもしれない
-const DEFAULT_FROM = '食事でつながる「shokujii」<shokujii@nijuni.jp>'
-const DEFAULT_TO = 'support+to@nijuni.jp'
-const SUPPORT_MAIL = 'shokujiiサポート<support+cc@nijuni.jp>'
+import { getCommunityUrl, getEventUrl, getOrderUrl, getUserUrl } from './utils/urls.js'
+import { DEFAULT_FROM, DEFAULT_TO, SUPPORT_MAIL } from './utils/mail.js'
 
 const ORDER_DEADLINE_TEMPLATE_ID = 'd-8609b6a7b1514595ae68d18532331e0e'
 const ORDER_DEADLINE_FOR_ORGANIZER_TEMPLATE_ID = 'd-1099d87af79f4d898012db3b8024715f'
@@ -74,22 +71,6 @@ function convertToDuration(startMillis, endMillis) {
   })
   const end = dateFns.format(endMillis, 'HH:mm')
   return `${start}〜${end}`
-}
-
-function getCommunityUrl(communityAccount) {
-  return `https://${process.env.EVENT_HOST}/c/${communityAccount}`
-}
-
-function getEventUrl(communityAccount, eventId) {
-  return `https://${process.env.EVENT_HOST}/c/${communityAccount}/e/${eventId}`
-}
-
-function getOrderUrl(eventId) {
-  return `https://${process.env.ADMIN_HOST}/order/${eventId}`
-}
-
-function getUserUrl(userId) {
-  return `https://${process.env.EVENT_HOST}/u/${userId}`
 }
 
 async function getShopForEvent(eventSnapshot) {
