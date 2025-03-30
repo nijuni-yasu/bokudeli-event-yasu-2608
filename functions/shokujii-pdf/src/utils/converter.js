9689faa901ae6c17660f6007eb52df48fa819dc7
Michiko Fukao <Michiko.F0429@gmail.com>
2025-03-30 20:58:49 +0900

    [functions pdf] HTMLのタグを削除する関数の作成

--- functions/shokujii-pdf/src/utils/converter.js
+++ functions/shokujii-pdf/src/utils/converter.js
@@ -12,3 +12,18 @@ export const convertDateRangeToString = (startDate, endDate) => {
   const endDateString = format(endDate, 'HH:mm')
   return `${startDateString} 〜 ${endDateString}`
 }
+
+// HTMLのタグを削除する関数
+export const convertHtmlToPlaneText = (html) => {
+  // <a>タグのリンクテキストを抽出し、リンクを削除
+  let textOnly = html.replace(/<a [^>]*>(.*?)<\/a>/gi, '$1')
+
+  // 残りのHTMLタグを削除
+  textOnly = textOnly.replace(/<[^>]+>/g, '')
+
+  // 不要な空白を削除
+  textOnly = textOnly.replace(/\s+/g, ' ')
+
+  // 前後の空白を削除
+  return textOnly.trim()
+}
