13e0bbfebd585d0e53e0855bf7fd4b12445a94e9
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-04-06 00:28:45 +0900

    [functions] リマインドメールにイベント管理画面の参加者一覧URLを送信する

--- functions/sendgrid-mail.js
+++ functions/sendgrid-mail.js
@@ -5,7 +5,7 @@ import ja from 'date-fns/locale/ja'
 import sgMail from '@sendgrid/mail'
 import { convertTruncateText } from './utils/converter.js'
 import { makeIcs } from './make-ics.js'
-import { getCommunityUrl, getEventUrl, getOrderUrl, getUserUrl } from './utils/urls.js'
+import { getCommunityUrl, getEventUrl, getOrderUrl, getUserUrl, getManageEventMemberUrl } from './utils/urls.js'
 import { DEFAULT_FROM, DEFAULT_TO, SUPPORT_MAIL } from './utils/mail.js'
 
 const ORDER_DEADLINE_TEMPLATE_ID = 'd-8609b6a7b1514595ae68d18532331e0e'
@@ -301,6 +301,7 @@ async function createTemplateDataForOrganizersOrderRemind(eventSnapshot, event_d
     ...eventData,
     event_days_ago,
     event_url: getEventUrl(eventData.community_account, eventSnapshot.id),
+    manage_event_member_url: getManageEventMemberUrl(eventSnapshot.id),
     event_deadline_datetime,
     event_datetime,
     orders,
