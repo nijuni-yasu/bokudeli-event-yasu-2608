9ad74b617a8e569da7d428a08482dd44be8c9257
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-04-12 22:59:05 +0900

    [base] イベントページのメニューカードをレスポンシブ対応する #943

--- base/src/components/EventMenuList.vue
+++ base/src/components/EventMenuList.vue
@@ -17,24 +17,25 @@ const emit = defineEmits<{
     <v-row v-if="menus !== null">
       <v-col v-for="(menu, i) of menus" :key="`menu_${i}`" md="4" sm="6" cols="12" class="pa-3">
         <v-card class="mb-1" color="text-center">
-          <v-img :src="menu.imageUrl ?? undefined" aspect-ratio="1" cover />
+          <v-row no-gutters>
+            <v-col cols="6" sm="12" class="d-flex">
+              <v-img :src="menu.imageUrl ?? undefined" aspect-ratio="1" cover />
+            </v-col>
 
-          <!-- title -->
-          <v-card-title class="justify-center pb-3 text-wrap">
-            {{ menu.name }}
-          </v-card-title>
-          <v-card-text class="text-left pb-8">
-            {{ menu.description }}
-          </v-card-text>
-          <v-card-text class="text-right pb-2">
-            <span style="font-size: 14px; color: #3a3541de">¥ </span>
-            <span style="font-size: 20px; color: #3a3541de">{{ priceString(menu.price) }}</span>
-          </v-card-text>
-          <v-row class="justify-center">
-            <v-col class="text-center">
+            <v-col cols="6" sm="12" class="pa-2 d-flex flex-column">
+              <v-card-title class="justify-start text-wrap px-1 py-0 pa-sm-1">
+                {{ menu.name }}
+              </v-card-title>
+              <v-card-text class="text-left text-subtitle-2 text-wrap text-sm-no-wrap px-1 py-0 pa-sm-1">
+                {{ menu.description }}
+              </v-card-text>
+              <v-spacer />
+              <v-card-text class="text-right px-1 pt-2 pb-0 pa-sm-1">
+                <span style="font-size: 14px; color: #3a3541de">¥ </span>
+                <span style="font-size: 20px; color: #3a3541de">{{ priceString(menu.price) }}</span>
+              </v-card-text>
               <v-btn
-                style="font-size: 16px"
-                class="mt-2 mb-5"
+                class="menu-button"
                 :class="{ 'disable-menu-button': disabled || menu.isSoldout === true }"
                 color="primary"
                 rounded="pill"
@@ -64,5 +65,26 @@ const emit = defineEmits<{
 <style lang="scss" scoped>
 .disable-menu-button {
   opacity: 0.6;
+}
+
+@media (max-width: 600px) {
+  .menu-button {
+    font-size: 13px !important;
+    height: 30px !important;
+    margin: 10px 10px 5px 10px !important;
+  }
+  .text-sm-no-wrap {
+    white-space: nowrap !important;
+    overflow: hidden !important;
+    text-overflow: ellipsis !important;
+  }
+}
+
+@media (min-width: 601px) {
+  .menu-button {
+    font-size: 14px !important;
+    height: 32px !important;
+    margin: 10px 20px 5px 20px !important;
+  }
 }
 </style>
