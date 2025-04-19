f1df6f8b497173eb957fefb92e7b02e0b1c0bd0b
Hayato Yokota <yokochie@gmail.com>
2025-04-19 20:47:41 +0900

    ChannelService plugin key が設定されていないときにメッセージを出力するように変更

--- user/src/channelIo.ts
+++ user/src/channelIo.ts
@@ -2,4 +2,10 @@ import * as ChannelService from '@channel.io/channel-web-sdk-loader'
 
 // チャネルトークスクリプト読み込み
 ChannelService.loadScript()
-ChannelService.boot({ pluginKey: import.meta.env.VITE_CHANNEL_IO_PLUGIN_KEY, hideChannelButtonOnBoot: true })
+
+const pluginKey = import.meta.env.VITE_CHANNEL_IO_PLUGIN_KEY
+if (pluginKey) {
+  ChannelService.boot({ pluginKey: import.meta.env.VITE_CHANNEL_IO_PLUGIN_KEY, hideChannelButtonOnBoot: true })
+} else {
+  console.error('ChannelService plugin key is not defined')
+}
