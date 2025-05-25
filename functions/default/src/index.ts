52b173fdf3a1e977579aef81329a7e69b440fc06
Yusuke Nakashita <kokufu.ym@gmail.com>
2025-05-25 15:10:48 +0900

    [functions][user][curry] #1103 communityManager を functions v2 対応に修正

--- functions/default/src/index.ts
+++ functions/default/src/index.ts
@@ -13,7 +13,7 @@ setGlobalOptions({ region: 'asia-northeast1' })
 
 // ES module 形式の import は並列読み込みのため、initializeApp 等の呼び出しが先に行われることを保証できない
 // そのため、ここでは 動的 import を使い、静的 import より後に実行されることを保証する
-const [{ get_invitaion_url_for_community_manager, accept_invitation_for_community_manager }] = await Promise.all([
-  import('./community-manager.js'),
+const [{ getInvitaionUrlForCommunityManager, acceptInvitationForCommunityManager }] = await Promise.all([
+  import('./communityManager.js'),
 ])
-export { get_invitaion_url_for_community_manager, accept_invitation_for_community_manager }
+export { getInvitaionUrlForCommunityManager, acceptInvitationForCommunityManager }
