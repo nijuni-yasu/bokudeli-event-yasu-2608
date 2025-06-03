2fd3db632de5e65f20f7e74dd4a82419cdb28f5b
Yusuke Nakashita <kokufu.ym@gmail.com>
2025-06-03 13:49:10 +0900

    [functions] Copy utils/mail from legacy to default

---
+++ functions/default/src/utils/mail.ts
@@ -0,0 +1,4 @@
+// 環境変数の方がよいかもしれない
+export const DEFAULT_FROM = '食事でつながる「shokujii」<shokujii@nijuni.jp>'
+export const DEFAULT_TO = 'support+to@nijuni.jp'
+export const SUPPORT_MAIL = 'shokujiiサポート<support+cc@nijuni.jp>'
