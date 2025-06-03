2726c688717a0c8f074a5c2e1fa37449f0720eb4
Yusuke Nakashita <kokufu.ym@gmail.com>
2025-06-03 13:17:24 +0900

    [common] urls に現行 legacy で使用しているすべての URL を追加

--- common/src/utils/urls.ts
+++ common/src/utils/urls.ts
@@ -1,3 +1,40 @@
+/*
+ * User URLs
+ */
+export function getCommunityUrl(host: string, communityAccount: string) {
+  return `https://${host}/c/${communityAccount}`
+}
+
+export function getEventUrl(host: string, communityAccount: string, eventId: string) {
+  return `https://${host}/c/${communityAccount}/e/${eventId}`
+}
+
+export function getUserUrl(host: string, userId: string) {
+  return `https://${host}/u/${userId}`
+}
+
 export const getCommunityInvitationUrl = (host: string, communityAccount: string, tokenId: string): string => {
   return `https://${host}/c/${communityAccount}/invites?t=${tokenId}`
+}
+
+/*
+ * User Manage URLs
+ */
+export function getManageEventMemberUrl(host: string, eventId: string) {
+  return `https://${host}/manage/event/${eventId}/member`
+}
+
+export function getManageEventInvoiceUrl(host: string, eventId: string, invoiceId: string) {
+  return `https://${host}/manage/event/${eventId}/invoice?id=${invoiceId}`
+}
+
+export function getManageCommunityUrl(host: string, communityAccount: string) {
+  return `https://${host}/manage/community/${communityAccount}/events`
+}
+
+/*
+ * Admin URLs
+ */
+export function getAdminOrderUrl(host: string, eventId: string) {
+  return `https://${host}/order/${eventId}`
 }
