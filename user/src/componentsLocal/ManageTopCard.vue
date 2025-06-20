85cd518c4b3a8789e79267acd78ea23d3bad9e62
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-06-20 19:55:39 +0900

    [user] #1192 アイコン画像を追加

--- user/src/componentsLocal/ManageTopCard.vue
+++ user/src/componentsLocal/ManageTopCard.vue
@@ -4,22 +4,33 @@ interface Props {
   title: string
   description: string
   imageSrc: string
+  buttonText?: string
+  buttonLink?: string
+  buttonHref?: string
 }
 
 defineProps<Props>()
 </script>
 
 <template>
-  <v-col cols="12" md="3" sm="6" class="">
+  <v-col cols="12" md="3" sm="6" class="pa-2">
     <v-card elevation="0" class="card-bg ma-0 pa-0">
-      <v-card-text>
-        <span class="text-h5 font-weight-bold">{{ stepNumber }} {{ title }}</span>
+      <v-card-text class="px-0 text-center card-title">
+        <span>{{ stepNumber }}{{ title }}</span>
       </v-card-text>
       <div class="text-center d-flex justify-center">
-        <v-img width="150px" height="150px" :src="imageSrc"/>
+        <v-img width="110px" height="110px" :src="imageSrc"/>
       </div>
       <v-card-text class="text-subtitle-2">
         {{ description }}
+      </v-card-text>
+      <v-card-text class="d-flex justify-center">
+        <v-btn v-if="buttonLink" class="ma-1" color="primary" variant="outlined" :to="buttonLink">
+          {{ buttonText }}
+        </v-btn>
+        <v-btn v-if="buttonHref" class="ma-1" color="primary" variant="outlined" :href="buttonHref" target="_blank">
+          {{ buttonText }}
+        </v-btn>
       </v-card-text>
     </v-card>
   </v-col>
@@ -27,8 +38,11 @@ defineProps<Props>()
 <style scoped>
 .card-bg {
   background-color: #F9FbFe;
-  /* background-color: #fff; */
   border: 1px solid #e0e0e0;
+}
+.card-title {
+  font-size:16px;
+  font-weight: bold;
 }
 </style>
 
--- user/src/pages/manage/index.vue
+++ user/src/pages/manage/index.vue
@@ -1,90 +1,167 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { mdiHome } from '@mdi/js'
-import community from '@/assets/images/manage/community_01.png'
 import ManageTopCard from '@/componentsLocal/ManageTopCard.vue'
+import about from '@/assets/images/manage_top/about_shokujii.png'
+import howto01 from '@/assets/images/manage_top/manage_howto_icon_01.png'
+import howto02 from '@/assets/images/manage_top/manage_howto_icon_02.png'
+import howto03 from '@/assets/images/manage_top/manage_howto_icon_03.png'
+import howto04 from '@/assets/images/manage_top/manage_howto_icon_04.png'
+import howto05 from '@/assets/images/manage_top/manage_howto_icon_05.png'
+import howto06 from '@/assets/images/manage_top/manage_howto_icon_06.png'
+import howto07 from '@/assets/images/manage_top/manage_howto_icon_07.png'
+import howto08 from '@/assets/images/manage_top/manage_howto_icon_08.png'
+import func01 from '@/assets/images/manage_top/manage_func_icon_01.png'
+import func02 from '@/assets/images/manage_top/manage_func_icon_02.png'
+import func03 from '@/assets/images/manage_top/manage_func_icon_03.png'
+import func04 from '@/assets/images/manage_top/manage_func_icon_04.png'
+import func05 from '@/assets/images/manage_top/manage_func_icon_05.png'
+import func06 from '@/assets/images/manage_top/manage_func_icon_06.png'
+import func07 from '@/assets/images/manage_top/manage_func_icon_07.png'
+import func08 from '@/assets/images/manage_top/manage_func_icon_08.png'
+import func09 from '@/assets/images/manage_top/manage_func_icon_09.png'
+import price01 from '@/assets/images/manage_top/manage_price_icon_01.png'
+import price02 from '@/assets/images/manage_top/manage_price_icon_02.png'
+import support01 from '@/assets/images/manage_top/manage_support_icon_01.png'
+import support02 from '@/assets/images/manage_top/manage_support_icon_02.png'
+import support03 from '@/assets/images/manage_top/manage_support_icon_03.png'
 
-const router = useRouter()
-// router.push('/manage/community')
 
 const howtos = [
   {
     stepNumber: '①',
-    title: 'コミュニティを作成しよう',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'コミュニティを作成',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: 'コミュニティを作成',
+    buttonLink: '/manage/newcommunity',
+    imageSrc: howto01
   },
   {
     stepNumber: '②',
     title: 'イベントを下書き作成',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: 'イベントを作成',
+    buttonLink: '/manage/event',
+    imageSrc: howto02
   },
   {
     stepNumber: '③',
-    title: 'お店に予約申請しよう',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'お店に予約申請',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: howto03
   },
   {
     stepNumber: '④',
     title: 'まずは主催者が注文！',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: howto04
   },
   {
     stepNumber: '⑤',
-    title: 'SNSなどで告知開始',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'SNSやメールで告知',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: howto05
   },
   {
     stepNumber: '⑥',
-    title: 'ユーザーへの一斉配信',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '注文期限で自動発注',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: howto06
   },
   {
     stepNumber: '⑦',
-    title: '注文期限になると注文確定',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'お店からデリバリー',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: howto07
   },
   {
     stepNumber: '⑧',
-    title: 'いただきます&ごちそうさま',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'いただきます！',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: howto08
   }
 ]
 
 const functions = [
   {
     stepNumber: '①',
-    title: 'SNS投稿機能',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '参加者 事前決済',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func01
   },
   {
     stepNumber: '②',
-    title: '公開設定/URL限定公開',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '主催者 請求書払い',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func02
   },
   {
     stepNumber: '③',
-    title: '参加者決済/主催者請求書払い',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '公開設定/URL限定公開',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func03
   },
   {
     stepNumber: '④',
-    title: 'レター機能(メール配信)',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '管理者追加/削除',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func04
   },
   {
     stepNumber: '⑤',
-    title: 'チラシ生成機能',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'SNS投稿機能',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func05
   },
   {
     stepNumber: '⑥',
-    title: 'Slack連携',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'レター機能(メール配信)',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func06
   },
   {
     stepNumber: '⑦',
+    title: 'チラシ生成機能',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func07
+  },
+  {
+    stepNumber: '⑧',
+    title: 'Slack連携',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func08
+  },
+  {
+    stepNumber: '⑨',
     title: 'and so on...',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    imageSrc: func09
   }
 
 ]
@@ -92,36 +169,54 @@ const functions = [
 const prices = [
   {
     stepNumber: '①',
-    title: 'コミュニティ利用',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '主催者利用料 無料',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: price01
   },
   {
     stepNumber: '②',
-    title: 'コーポレート利用',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '配送料 無料',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: price02
   },
   {
     stepNumber: '③',
-    title: 'エンタープライズ利用',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '請求書払い手数料',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: func02
   },
 ]
 
 const supports = [
   {
     stepNumber: '①',
-    title: 'SNS投稿機能',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'チャットでサポート',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: support01
   },
   {
     stepNumber: '②',
-    title: '公開設定/URL限定公開',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: 'オンラインMTG',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: support02
   },
   {
     stepNumber: '③',
-    title: '参加者決済/主催者請求書払い',
-    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文'
+    title: '緊急連絡先',
+    description: '説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文・説明文',
+    buttonText: '詳細を見る',
+    buttonHref: 'https://bit.ly/3S3L8Sv',
+    imageSrc: support03
   },
 ]
 </script>
@@ -133,15 +228,19 @@ const supports = [
         <v-container class="my-15">
           <v-row class="text-center">
             <v-col cols="12">
-              <v-card-title>
+              <v-card-text>
                 <span class="text-h2 font-weight-bold">【shokujiiって？】</span>
-              </v-card-title>
+              </v-card-text>
               <v-card-text>
                 <span class="text-h5">
-                  イベント開催の流れをご覧ください。イベント開催の流れをご覧ください。
+                  食事でつながる「shokujii」は「イベント集客ツール」に「フードオーダーシステム」を組み合わせたWEBサービスです。<br>
+                  イベント主催者は、注文するお店を選ぶだけで、簡単にイベントのコンテンツを作ることができます。<br>
+                  さらにイベント参加者は、食べたいメニューを選んで事前決済するだけでそのコミュニティの輪に加わることができます。<br>
+                  食のもとに人が集い、人のもとにさらに人が集う。食を通じた出会いの場を作ります。<br>
+                  <a href="https://about.shokujii.jp/" target="_blank">https://about.shokujii.jp/</a>
                 </span>
-                <div class="text-center d-flex justify-center pa-5">
-                  <v-img width="600px" height="300px" :src="community"/>
+                <div class="text-center d-flex justify-center pa-1">
+                  <v-img width="600px" height="300px" :src="about"/>
                 </div>
               </v-card-text>
             </v-col>
@@ -150,9 +249,9 @@ const supports = [
         <v-container class="my-15">
           <v-row class="text-center">
             <v-col cols="12">
-              <v-card-title>
-                <span class="text-h2 font-weight-bold">【イベントのながれ】</span>
-              </v-card-title>
+              <v-card-text>
+                <span class="text-h2 font-weight-bold">【イベント開催の流れ】</span>
+              </v-card-text>
               <v-card-text>
                 <span class="text-h5">
                   イベント開催の流れをご覧ください。イベント開催の流れをご覧ください。
@@ -167,16 +266,19 @@ const supports = [
               :step-number="step.stepNumber"
               :title="step.title"
               :description="step.description"
-              :image-src="community"
+              :image-src="step.imageSrc"
+              :button-text="step?.buttonText"
+              :button-link="step?.buttonLink"
+              :button-href="step?.buttonHref"
             />
           </v-row>
         </v-container>
         <v-container class="my-15">
           <v-row class="text-center">
             <v-col cols="12">
-              <v-card-title>
+              <v-card-text>
                 <span class="text-h2 font-weight-bold">【機能一覧】</span>
-              </v-card-title>
+              </v-card-text>
               <v-card-text>
                 <span class="text-h5">
                   shokujiiでできることをご覧ください。shokujiiでできることをご覧ください。
@@ -191,19 +293,21 @@ const supports = [
               :step-number="step.stepNumber"
               :title="step.title"
               :description="step.description"
-              :image-src="community"
+              :image-src="step.imageSrc"
+              :button-text="step?.buttonText"
+              :button-href="step?.buttonHref"
             />
           </v-row>
         </v-container>
         <v-container class="my-15">
           <v-row class="text-center">
             <v-col cols="12">
-              <v-card-title>
+              <v-card-text>
                 <span class="text-h2 font-weight-bold">【利用料金】</span>
-              </v-card-title>
+              </v-card-text>
               <v-card-text>
                 <span class="text-h5">
-                  無料でご利用いただけます。
+                 基本無料でご利用いただけます。
                 </span>
               </v-card-text>
             </v-col>
@@ -215,16 +319,18 @@ const supports = [
               :step-number="step.stepNumber"
               :title="step.title"
               :description="step.description"
-              :image-src="community"
+              :image-src="step.imageSrc"
+              :button-text="step?.buttonText"
+              :button-href="step?.buttonHref"
             />
           </v-row>
         </v-container>
         <v-container class="my-15">
           <v-row class="text-center">
             <v-col cols="12">
-              <v-card-title>
+              <v-card-text>
                 <span class="text-h2 font-weight-bold">【サポート】</span>
-              </v-card-title>
+              </v-card-text>
               <v-card-text>
                 <span class="text-h5">
                   サポートにご連絡ください。
@@ -239,7 +345,9 @@ const supports = [
               :step-number="step.stepNumber"
               :title="step.title"
               :description="step.description"
-              :image-src="community"
+              :image-src="step.imageSrc"
+              :button-text="step?.buttonText"
+              :button-href="step?.buttonHref"
             />
           </v-row>
         </v-container>
