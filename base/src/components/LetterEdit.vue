d1dcf6ff0127bda2a073ecd6afa7d45ff437f3c9
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-06-22 23:10:34 +0900

    [base] #1203 テスト送信ボタンを連打できてしまう不具合の修正。送信中はloading表示する

--- base/src/components/LetterEdit.vue
+++ base/src/components/LetterEdit.vue
@@ -48,6 +48,7 @@ const isSubmitDisabled = computed(() => {
 
 const isScheduled = ref(false)
 const scheduleTime = ref(Timestamp.now())
+const isTestSending = ref(false)
 
 const scheduledDate = computed({
   get: () => dateString(scheduleTime.value.toDate() ?? null),
@@ -104,6 +105,7 @@ const save = async () => {
 }
 const sendTest = async () => {
   try {
+    isTestSending.value = true
     await _save()
     // ! は本来使うべきではないが、sendTest が呼ばれるケースでは必ず community は存在することが保証されている
     // TODO store の方で存在を保証する仕組みを用意する
@@ -112,6 +114,8 @@ const sendTest = async () => {
   } catch (e) {
     console.error(e)
     notification.show($t('manage.letter.edit.send_test_error'), 'error')
+  } finally {
+    isTestSending.value = false
   }
 }
 </script>
@@ -221,7 +225,7 @@ const sendTest = async () => {
             <v-btn @click="save" variant="outlined" :disabled="!isValid">
               {{ _letter.status === 'draft' ? $t('manage.letter.edit.save_draft') : $t('manage.letter.edit.to_draft') }}
             </v-btn>
-            <v-btn v-if="_letter.status === 'draft'" @click="sendTest" :disabled="!isValid">
+            <v-btn v-if="_letter.status === 'draft'" @click="sendTest" :disabled="!isValid || isTestSending" :loading="isTestSending">
               {{ $t('manage.letter.edit.send_test') }}
             </v-btn>
             <v-btn @click="submit" :disabled="isSubmitDisabled">
