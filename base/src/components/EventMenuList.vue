64705604815eeb8430b1d8cd3799471d33b64574
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-07-02 04:45:11 +0900

    イベントのメニュー一覧のUIを調整

--- base/src/components/EventMenuList.vue
+++ base/src/components/EventMenuList.vue
@@ -14,37 +14,41 @@ const emit = defineEmits<{
 </script>
 <template>
   <section>
-    <v-row v-if="menus !== null">
+    <v-row v-if="menus !== null" class="align-items-stretch">
       <v-col v-for="(menu, i) of menus" :key="`menu_${i}`" md="4" sm="6" cols="12" class="pa-3">
-        <v-card class="mb-1" color="text-center">
+        <v-card height="100%" color="text-center">
           <v-row no-gutters>
             <v-col cols="6" sm="12" class="d-flex">
               <v-img :src="menu.imageUrl ?? undefined" aspect-ratio="1" cover />
             </v-col>
 
             <v-col cols="6" sm="12" class="pa-2 d-flex flex-column">
-              <v-card-title class="justify-start text-wrap px-1 py-0 pa-sm-1">
+              <v-card-title class="justify-start text-h6 font-weight-bold text-wrap pa-1">
                 {{ menu.name }}
               </v-card-title>
-              <v-card-text class="text-left text-subtitle-2 text-wrap text-sm-no-wrap px-1 py-0 pa-sm-1">
+              <v-card-text class="text-left text-subtitle-2 px-1 py-0 description-text">
                 {{ menu.description }}
               </v-card-text>
-              <v-spacer />
-              <v-card-text class="text-right px-1 pt-2 pb-0 pa-sm-1">
-                <span style="font-size: 14px; color: #3a3541de">¥ </span>
-                <span style="font-size: 20px; color: #3a3541de">{{ priceString(menu.price) }}</span>
+              <v-card-text class="text-right pa-0 ma-3">
+                <span class="yen-text">¥ </span>
+                <span class="price-text">{{ priceString(menu.price) }}</span>
               </v-card-text>
-              <v-btn
-                class="menu-button"
-                :class="{ 'disable-menu-button': disabled || menu.isSoldout === true }"
-                color="primary"
-                rounded="pill"
-                elevation="5"
-                :prepend-icon="mdiFoodForkDrink"
-                @click="emit('selectMenu', menu)"
-              >
-                {{ menu.isSoldout === true ? '売り切れ' : '注文して参加する' }}
-              </v-btn>
+              <v-row class="pb-1 px-2">
+                <v-col cols="12">
+                  <v-btn
+                    class="menu-button"
+                    block
+                    :class="{ 'disable-menu-button': disabled || menu.isSoldout === true }"
+                    color="primary"
+                    rounded="pill"
+                    elevation="5"
+                    :prepend-icon="mdiFoodForkDrink"
+                    @click="emit('selectMenu', menu)"
+                  >
+                    {{ menu.isSoldout === true ? '売り切れ' : '注文して参加する' }}
+                  </v-btn>
+                </v-col>
+              </v-row>
             </v-col>
           </v-row>
         </v-card>
@@ -66,12 +70,25 @@ const emit = defineEmits<{
 .disable-menu-button {
   opacity: 0.6;
 }
+.description-text {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+.price-text {
+  font-size: 22px;
+  color: #3a3541de;
+}
+.yen-text {
+  font-size: 16px;
+  color: #3a3541de;
+}
 
 @media (max-width: 600px) {
   .menu-button {
     font-size: 13px !important;
     height: 30px !important;
-    margin: 10px 10px 5px 10px !important;
   }
   .text-sm-no-wrap {
     white-space: nowrap !important;
@@ -84,7 +101,6 @@ const emit = defineEmits<{
   .menu-button {
     font-size: 14px !important;
     height: 32px !important;
-    margin: 10px 20px 5px 20px !important;
   }
 }
 </style>
