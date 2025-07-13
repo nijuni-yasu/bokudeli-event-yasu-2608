8a90c25339f6abda58f92ea497ab572f0baf1abc
Hayato Yokota <yokochie@gmail.com>
2025-07-13 20:52:36 +0900

    [function] #1274 sendInvoiceMailToOrganizers の移植を取りやめ

--- functions/default/src/eventConclusionMail.ts
+++ functions/default/src/eventConclusionMail.ts
@@ -4,11 +4,9 @@ import { getEventUrl, getManageEventInvoiceUrl } from './utils/urls.js'
 import { convertToDateWeekdayShort } from './commonUtils/datetime.js'
 import { getUserPersonalInformation } from './stores/user.js'
 import { getAcceptingOrderEventsByEndTime, type ShokujiiEvent } from './stores/event.js'
-const { createEventBillInvoice } = require('../../legacy/src/eventBillInvoice.js')
 
 // テンプレートID
 const EVENT_SURVEY_TEMPLATE_ID = 'd-6ad8131506164c2f864155182c63de2d'
-const EVENT_INVOICE_TEMPLATE_ID = 'd-48e3179255834b8bb895cd995b1aac28'
 
 // 型定義
 interface EventConcludedTemplateData {
@@ -17,13 +15,6 @@ interface EventConcludedTemplateData {
   event_cover_url: string
   event_url: string
   is_public: boolean
-}
-
-interface InvoiceTemplateData {
-  company: string
-  person: string
-  event_name: string
-  event_invoice_url: string
 }
 
 /**
@@ -73,52 +64,5 @@ export async function sendEventConcludedMailToMembers(start: number, end: number
         console.warn('Failed to send event concluded mail to members:', err)
       }
     }),
-  )
-}
-
-/**
- * イベント終了時に主催者に請求書メールを送信
- */
-export async function sendInvoiceMailToOrganizers(start: number, end: number): Promise<void[]> {
-  const events = await getAcceptingOrderEventsByEndTime(start, end)
-
-  return Promise.all(
-    events
-      .filter((event) => event.event_payment === 'community_bill')
-      .map(async (event) => {
-        try {
-          const { getFirestore } = await import('firebase-admin/firestore')
-          const db = getFirestore()
-          const communitySnapshot = await db.collection('communities').doc(event.community_id).get()
-          const eventSnapshot = await db
-            .collection('communities')
-            .doc(event.community_id)
-            .collection('events')
-            .doc(event.id)
-            .get()
-          const invoiceId = await createEventBillInvoice(communitySnapshot, eventSnapshot)
-
-          const dynamic_template_data: InvoiceTemplateData = {
-            company: event.organizer_company || '',
-            person: event.bill_fullname || '',
-            event_name: event.event_name || '',
-            event_invoice_url: getManageEventInvoiceUrl(event.id, invoiceId),
-          }
-
-          const to = event.bill_email
-          const cc = event.organizer_email
-
-          await sgMail.send({
-            to,
-            from: DEFAULT_FROM,
-            cc: to !== cc ? cc : undefined,
-            bcc: SUPPORT_MAIL,
-            templateId: EVENT_INVOICE_TEMPLATE_ID,
-            dynamicTemplateData: dynamic_template_data,
-          })
-        } catch (err) {
-          console.warn('Failed to send invoice mail to organizers:', JSON.stringify(err))
-        }
-      }),
   )
 }
--- functions/default/src/pollingTask.ts
+++ functions/default/src/pollingTask.ts
@@ -5,7 +5,7 @@ import {
   sendOrderDeadlineMailToOrganizers,
   sendOrderDeadlineMailToMembers,
 } from './orderDeadlineMail.js'
-import { sendEventConcludedMailToMembers, sendInvoiceMailToOrganizers } from './eventConclusionMail.js'
+import { sendEventConcludedMailToMembers } from './eventConclusionMail.js'
 
 const ONE_DAY_MILLIS = 24 * 60 * 60 * 1000
 
@@ -28,7 +28,6 @@ export const pollingTask = onSchedule(
       sendOrderDeadlineMailToOrganizers(start, end),
       sendOrderDeadlineMailToMembers(start, end),
       sendEventConcludedMailToMembers(start, end),
-      sendInvoiceMailToOrganizers(start, end),
     ]
     await Promise.all(promiseFunctions)
   },
