ee26a36daa7a69a361aec74bd2017fc552db16da
keita okuda <keitaokuda.0925@gmail.com>
2025-07-18 23:27:35 +0900

    [common] UserPersonalInformation schema更新

--- common/src/schemas/UserPersonalInformation.ts
+++ common/src/schemas/UserPersonalInformation.ts
@@ -2,12 +2,20 @@ import { z } from 'zod'
 
 const UserPersonalInformationDbSchema = z.object({
   user_email: z.string().email(),
+  user_email_pending: z.string(),
+  user_sns_google: z.string(),
+  user_sns_twitter_access_token: z.string(),
+  user_sns_twitter_secret: z.string(),
 })
 
 // id 以外は デフォルト なので、 Schema は必要ない
 export class UserPersonalInformation {
   readonly id: string
   user_email: string = ''
+  user_email_pending: string = ''
+  user_sns_google: string = ''
+  user_sns_twitter_access_token: string = ''
+  user_sns_twitter_secret: string = ''
 
   constructor(id: string, src: Partial<UserPersonalInformation>) {
     Object.assign(this, src)
