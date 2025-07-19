d0fb2f41ac1e27015d0b8fe7d00601ad5acbbdf0
keita okuda <keitaokuda.0925@gmail.com>
2025-07-19 15:13:55 +0900

    [user] 条件式を単純化

--- user/src/pages/u/[userId].vue
+++ user/src/pages/u/[userId].vue
@@ -269,7 +269,7 @@ const downloadInvoice = async (order: OrderItem) => {
     v-model="isUserSuccessJoinEventDialogVisible"
     :event-id="(($route.query.eventId ?? '') as string)"
     :community-account="(($route.query.communityAccount ?? '') as string)"
-    :is-posted="($route.query.isPosted === 'true' ? true : false)"
+    :is-posted="($route.query.isPosted === 'true')"
   />
 </template>
 
