dfd81973420e3cc076d466a694b20a84b32469c9
Yusuke Nakashita <kokufu.ym@gmail.com>
2025-07-19 21:17:05 +0900

    [admin] Apply ESLint

--- admin/src/navigation/utils.ts
+++ admin/src/navigation/utils.ts
@@ -8,4 +8,4 @@ export const getOrderDetailPath = (eventId: string) => `/order/${eventId}`
 export const getUserEventUrl = (communityId: string, eventId: string) =>
   `${import.meta.env.VITE_ORIGIN_HOST}/c/${communityId}/e/${eventId}`
 export const getUserUrl = (userId: string) => `${import.meta.env.VITE_ORIGIN_HOST}/u/${userId}`
-export const getNamesPrintPath = () => `/namesprint`
+export const getNamesPrintPath = () => '/namesprint'
