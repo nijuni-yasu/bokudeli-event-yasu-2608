b2c79c02722ffa9691b807205c602a6f507dd8af
Yusuke Nakashita <kokufu.ym@gmail.com>
2025-12-09 13:09:16 +0900

    [user] Delete unused function `isUserLoggedIn`

--- user/src/router/utils.ts
+++ user/src/router/utils.ts
@@ -1,10 +1,3 @@
-/**
- * Return if user is logged in
- * This is completely up to you and how you want to store the token in your frontend application
- * e.g. If you are using cookies to store the application please update this function
- */
-export const isUserLoggedIn = () => !!(localStorage.getItem('userData') && localStorage.getItem('accessToken'))
-
 export const getHomePath = () => '/'
 export const getCommunityListPath = () => '/communitylist'
 export const getCommunityPath = (communityAccount: string) => `/c/${communityAccount}`
