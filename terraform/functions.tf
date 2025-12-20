59286abcef1c5847eddd212acfe8a7904c74248e
nijuni-yasu <naohiro.yasukawa@gmail.com>
2025-12-20 21:38:42 +0900

    #1585 Twitterアクセストークン関連フィールドを削除、postToTwitter関数を削除
    ## 問題
    同一メールアドレスでXアカウント連携を行うと、以下のzodエラーが発生していた:
    - user_sns_twitter_access_token と user_sns_twitter_secret が null になる
    - メールアドレスが登録されているのにフィールドに表示されない
    - Xのアカウント連携をしなおしても症状が改善しない
    
    原因は、Firestoreに null が保存されている状態で、zodスキーマが
    z.string().default('') として定義されており、null を許容していなかったため。
    
    ## 対応内容
    1. UserPersonalInformationスキーマから削除
       - user_sns_twitter_access_token と user_sns_twitter_secret を
         UserPersonalInformationDbSchema と UserPersonalInformationAppSchema から削除
       - クラスのプロパティからも削除
       - 不要になった NonEmptyStringSchema のインポートを削除
    
    2. postToTwitter関数を削除
       - functions/default/src/postToTwitter.ts を削除
       - functions/default/src/index.ts から postToTwitter のエクスポートと
         インポートを削除
       - フロントエンドからの呼び出しが存在しないことを確認済み
    
    3. ドキュメントを更新
       - documents/00_仕様概要/01_仕様概要.md から該当フィールドの記載を削除
    
    4. Terraformリソースを削除
       - terraform/functions.tf から TWITTER_CONSUMER_KEY と
         TWITTER_CONSUMER_SECRET の Secret Manager リソース定義を削除
    
    ## 影響範囲
    - Firestoreに既存の null 値が残っていても問題なし
      (zodスキーマから削除されているため、読み込み時に無視される)
    - 次回 terraform apply 実行時に Secret Manager のリソースも削除される
    - 既存のXアカウント連携機能（Firebase Auth経由）には影響なし
    
    ## 動作確認
    - user_sns_twitter_access_token と user_sns_twitter_secret の
      参照がコードベースに残っていないことを確認済み
    - postToTwitter 関数の呼び出し箇所が存在しないことを確認済み
    - Lintエラーなし

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -8,25 +8,3 @@ resource "google_secret_manager_secret" "SENDGRID_API_KEY" {
     google_project_service.default
   ]
 }
-
-resource "google_secret_manager_secret" "TWITTER_CONSUMER_KEY" {
-  secret_id = "TWITTER_CONSUMER_KEY"
-  replication {
-    auto {}
-  }
-
-  depends_on = [
-    google_project_service.default
-  ]
-}
-
-resource "google_secret_manager_secret" "TWITTER_CONSUMER_SECRET" {
-  secret_id = "TWITTER_CONSUMER_SECRET"
-  replication {
-    auto {}
-  }
-
-  depends_on = [
-    google_project_service.default
-  ]
-}
